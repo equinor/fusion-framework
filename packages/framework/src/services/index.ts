d7f2ede16573baf846ba73ca48ba273695eaa482
odin thomas rochmann <odin.rochmann@gmail.com>
2021-06-18 13:56:06 +0200

    fix: expose auth creation

--- packages/framework/src/services/index.ts
+++ packages/framework/src/services/index.ts
@@ -4,3 +4,6 @@ export { default } from './create-services';
 
 export { HttpClientMsal, HttpClient } from './http';
 export type { HttpClientMsal as FusionClient } from './http';
+
+// TODO remove this later
+export { createAuthClient } from '@equinor/fusion-web-msal';
