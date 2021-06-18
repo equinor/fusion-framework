69821b9279d7b285320b880494fcd3122c3c8041
odin thomas rochmann <odin.rochmann@gmail.com>
2021-06-18 14:00:49 +0200

    fix: removed exposed msal creator

--- packages/framework/src/services/index.ts
+++ packages/framework/src/services/index.ts
@@ -4,6 +4,3 @@ export { default } from './create-services';
 
 export { HttpClientMsal, HttpClient } from './http';
 export type { HttpClientMsal as FusionClient } from './http';
-
-// TODO remove this later
-export { createAuthClient } from '@equinor/fusion-web-msal';
