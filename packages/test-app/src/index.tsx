98f2724f048af96acf839cc63117c70aad4ac5d7
odin thomas rochmann <odin.rochmann@gmail.com>
2022-01-10 10:12:52 +0100

    feat: add test app
    test imports and type-hinting

---
+++ packages/test-app/src/index.tsx
@@ -0,0 +1,13 @@
+import { createApp, AppConfigurator } from '@equinor/fusion-react-app';
+
+import serviceDiscovery, {
+    ServiceDiscoveryModule,
+} from '@equinor/fusion-framework-module-service-discovery';
+
+const App = () => <p>ok</p>;
+
+const config: AppConfigurator<[ServiceDiscoveryModule]> = (config) => {
+    config.serviceDiscovery.clientKey;
+};
+
+export const render = createApp(App, config, [serviceDiscovery]);
