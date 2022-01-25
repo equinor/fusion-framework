f876acb11e19d7802a28f58ce7d70bc76f777c5e
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-01-25 12:46:29 +0100

    fix: memo http clients

--- packages/react-app/src/hooks/use-http-client.ts
+++ packages/react-app/src/hooks/use-http-client.ts
@@ -1,30 +1,21 @@
 /* eslint-disable @typescript-eslint/ban-types */
+import { useMemo } from 'react';
 import { useModuleContext } from '../modules';
 
 type OtherString = string & {};
-export type AppHttpClient = 'my-client';
+export type AppHttpClient = '';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useHttpClient = (name: AppHttpClient | OtherString) => {
     const module = useModuleContext();
-    if (module.http.hasClient(name)) {
-        return module.http.createClient(name);
-    }
-    throw Error(`no configured client for key [${name}]`);
+    const client = useMemo(() => {
+        if (module.http.hasClient(name)) {
+            return module.http.createClient(name);
+        }
+        throw Error(`no configured client for key [${name}]`);
+    }, [name]);
+    // TODO - abort on unmount?
+    return client;
 };
-
-// export const useHttpClient = (
-//     client: AppHttpClient | FrameworkHttpClient | OtherString
-// ): IHttpClient => {
-//     const framework = useFramework();
-//     const module = useModuleContext();
-//     if (module.http.hasClient(client)) {
-//         return module.http.createClient(client);
-//     }
-//     if (framework.modules.http.hasClient(client)) {
-//         return framework.modules.http.createClient(client);
-//     }
-//     throw Error(`no configured client for key [${client}]`);
-// };
 
 export default useHttpClient;
