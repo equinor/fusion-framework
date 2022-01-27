441facdc32f71391683b33db394f34e966772faf
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-01-27 11:02:30 +0100

    fix(react-app): expose http interface

--- packages/react-app/src/hooks/http.ts
+++ packages/react-app/src/hooks/http.ts
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/ban-types */
+import { HttpClientMsal, IHttpClient } from '@equinor/fusion-framework-module-http';
 import { useMemo } from 'react';
 import { useModuleContext } from '../modules';
 
 type OtherString = string & {};
 export type AppHttpClient = '';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const useHttpClient = (name: AppHttpClient | OtherString) => {
+export { IHttpClient, HttpClientMsal };
+
+export const useHttpClient = <TType extends IHttpClient = HttpClientMsal>(
+    name: AppHttpClient | OtherString
+): TType => {
     const module = useModuleContext();
     const client = useMemo(() => {
         if (module.http.hasClient(name)) {
@@ -15,7 +19,7 @@ export const useHttpClient = (name: AppHttpClient | OtherString) => {
         throw Error(`no configured client for key [${name}]`);
     }, [name]);
     // TODO - abort on unmount?
-    return client;
+    return client as unknown as TType;
 };
 
 export default useHttpClient;
