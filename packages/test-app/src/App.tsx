55dc5e369702a5c48546df3d5e233638c2900668
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-02-04 09:01:36 +0100

    chore(react-app): update test app

---
+++ packages/test-app/src/App.tsx
@@ -0,0 +1,71 @@
+import { Suspense } from 'react';
+
+import createApp, { AppConfigurator } from '@equinor/fusion-framework-react-app';
+import {
+    useCurrentUser,
+    useFramework,
+    useModuleContext,
+} from '@equinor/fusion-framework-react-app/hooks';
+import { AppList } from 'AppList';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+interface App {
+    key: string;
+    name: string;
+}
+
+const configCallback: AppConfigurator = async (appModuleConfig, frameworkApi) => {
+    console.debug(0, 'configuring app', frameworkApi, appModuleConfig);
+    await frameworkApi.modules.serviceDiscovery.configureClient('portal', appModuleConfig);
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+};
+
+const queryClient = new QueryClient();
+
+export const AppComponent = (): JSX.Element => {
+    const framework = useFramework();
+    const modules = useModuleContext();
+    const account = useCurrentUser();
+    // const client = useHttpClient();
+    // const queryClient = useMemo(() => {}, [client]);
+    return (
+        <div>
+            <h3>Current user</h3>
+            <code>
+                <pre>{JSON.stringify(account, null, 4)}</pre>
+            </code>
+            <h3>Registered modules in Framework</h3>
+            <ul>
+                {Object.keys(framework.modules).map((x) => (
+                    <li key={x}>{x}</li>
+                ))}
+            </ul>
+            <h3>Registered modules in App</h3>
+            <ul>
+                {Object.keys(modules).map((x) => (
+                    <li key={x}>{x}</li>
+                ))}
+            </ul>
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools initialIsOpen />
+                <AppList />
+            </QueryClientProvider>
+        </div>
+    );
+};
+
+export const configurator = createApp(AppComponent, configCallback);
+
+export const App = () => {
+    const framework = useFramework();
+    const Component = configurator(framework, {});
+    return (
+        <Suspense fallback={<h1>Loading App</h1>}>
+            <Component />
+        </Suspense>
+    );
+};
+
+export default App;
---
+++ packages/test-app/src/AppList.tsx
@@ -0,0 +1,43 @@
+import { useHttpClient } from '@equinor/fusion-framework-react-app/hooks';
+import type { IHttpClient, FetchRequestInit } from '@equinor/fusion-framework-react-app/hooks/http';
+import { useMemo } from 'react';
+import { useQuery } from 'react-query';
+
+interface App {
+    key: string;
+    name: string;
+}
+
+const selector = (x: Response): Promise<App[]> => {
+    return x.json();
+};
+
+const useAllApps = () => {
+    const client = useHttpClient('portal');
+    const fn = useMemo(() => getAllApps(client), [client]);
+    return useQuery('all_apps', ({ signal }) => fn({ signal, selector }));
+    //     client.fetchAsync('api/apps', { selector, signal })
+    // );
+};
+
+const getAllApps =
+    (client: IHttpClient) =>
+    (init: FetchRequestInit): Promise<App[]> => {
+        return client.fetchAsync('api/apps', { ...init, selector });
+    };
+
+export const AppList = () => {
+    const { data, isLoading } = useAllApps();
+    if (isLoading) {
+        return <span>Loading apps.....</span>;
+    }
+
+    return (
+        <div>
+            <h3>List of registered apps in fusion</h3>
+            <code>
+                <pre>{JSON.stringify(data, null, 4)}</pre>
+            </code>
+        </div>
+    );
+};
