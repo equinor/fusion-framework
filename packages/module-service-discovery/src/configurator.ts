8917e4e3053b824ac8d878b0bfbe6a22efd56c3b
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-02-15 13:10:19 +0100

    feat(module-service-discovery): allow custom service discovery
    + add option for providing selctor of service request
    + add default selector of service discovery
    + changed services from array to record
    
    - removed client id from provider
    
    Signed-off-by: Odin Thomas Rochmann <odin.rochmann@gmail.com>

--- packages/module-service-discovery/src/configurator.ts
+++ packages/module-service-discovery/src/configurator.ts
@@ -1,11 +1,29 @@
+import type { Environment, EnvironmentResponse } from './types';
+
 export interface IServiceDiscoveryConfigurator {
     /** name of HttpClient */
     clientKey: string;
     /** URI for discovery */
     uri: string;
+    /** Response selector */
+    selector: (response: Response) => Promise<Environment>;
 }
+
+const defaultSelector = async (response: Response): Promise<Environment> => {
+    const env = (await response.json()) as EnvironmentResponse;
+    const services = env.services.reduce((acc, service) => {
+        return Object.assign(acc, {
+            [service.key]: {
+                clientId: env.clientId,
+                defaultScopes: [env.clientId + '/.default'],
+            },
+        });
+    }, {});
+    return { ...env, services };
+};
 
 export class ServiceDiscoveryConfigurator implements IServiceDiscoveryConfigurator {
     clientKey = 'service_discovery';
     uri = '/_discovery/environments/current';
+    selector = defaultSelector;
 }
