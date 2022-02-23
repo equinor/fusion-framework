05235e75c3f908ee901f07dc338b6b916fc98eb0
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-02-23 13:18:52 +0100

    chore: update workflow

--- clean-up.js
+++ clean-up.js
@@ -4,6 +4,7 @@ const lockFile = require('./package-lock.json');
 for (const [name, info] of Object.entries(lockFile.packages)) {
   if (name.match(/^packages/)) {
     delete lockFile.dependencies[name];
+    delete lockFile.packages[name];
     delete lockFile.packages[info.name];
     delete lockFile.packages[`node_modules/${info.name}`];
     console.log(`removed from lock file [${name}]`);
