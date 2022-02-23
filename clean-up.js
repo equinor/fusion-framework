cbc2a4166fc2d3c0b6c6821e00e258398744757b
odinr <odin.rochmann@gmail.com>
2022-02-23 12:52:33 +0100

    build(release): update worksflow (#29)

---
+++ clean-up.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const lockFile = require('./package-lock.json');
+
+for (const [name, info] of Object.entries(lockFile.packages)) {
+  if (name.match(/^packages/)) {
+    delete lockFile.dependencies[name];
+    delete lockFile.packages[info.name];
+    delete lockFile.packages[`node_modules/${info.name}`];
+    console.log(`removed from lock file [${name}]`);
+  }
+}
+
+fs.writeFileSync('./package-lock.json', JSON.stringify(lockFile, null, 2));
