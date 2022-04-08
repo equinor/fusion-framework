d9d4c70e9eae3aa1724456c60d735e71acb97f15
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-04-08 18:01:13 +0200

    build: fix build clean file

--- clean-up.js
+++ clean-up.js
@@ -2,9 +2,16 @@ const fs = require('fs');
 const lockFile = require('./package-lock.json');
 
 const pattern = /^node_modules\/@equinor\/fusion-framework|^packages/;
-for (const dep of Object.keys(lockFile.packages).filter(x => x.match(pattern))) {
-  delete lockFile.packages[dep];
-  console.info(`removed from lock file [${dep}]`);
+try {
+  for (const dep of Object.keys(lockFile.packages).filter(x => x.match(pattern))) {
+    if (lockFile.packages[dep]) {
+      delete lockFile.packages[dep];
+      console.info(`removed from lock file [${dep}]`);
+    }
+  }
+} catch (err) {
+  console.error(err);
+} finally {
+  fs.writeFileSync('./package-lock.json', JSON.stringify(lockFile, null, 2));
 }
 
-fs.writeFileSync('./package-lock.json', JSON.stringify(lockFile, null, 2));
