fd6ea353e10acb99699431f3a70afff507d5092d
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-06-10 13:55:08 +0200

    feat(module): allow modules to have deps
    - delay initializing of module until dependent module is loaded
    - add logging

--- packages/module/src/initialize-modules.ts
+++ packages/module/src/initialize-modules.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { BehaviorSubject, delayWhen, filter, from, lastValueFrom, map, mergeMap, of } from 'rxjs';
+
 import type {
     AnyModule,
     ModulesConfig,
@@ -6,6 +8,90 @@ import type {
     ModulesConfigurator,
     ModulesInstanceType,
 } from './types';
+
+// TODO - move to own lib ;)
+class ConsoleLogger {
+    constructor(protected domain: string) {}
+
+    /** @inheritdoc */
+    protected _createMessage(msg: unknown[]): unknown[] {
+        return [
+            `%c FUSION FRAMEWORK %c ${this.domain} %c %s`,
+            'background: rgb(179, 13, 47); color: white; padding: 1px;',
+            'background: rgb(244, 244, 244); color: rgb(36, 55, 70); padding: 1px;',
+            'background: none; color: inherit',
+            ...msg.reduce((c: unknown[], n: unknown) => [...c, n, '\n'], []),
+        ];
+    }
+
+    debug(...msg: unknown[]) {
+        process.env.NODE_ENV === 'development' && console.debug(...this._createMessage(msg));
+    }
+    info(...msg: unknown[]) {
+        console.info(...this._createMessage(msg));
+    }
+    warn(...msg: unknown[]) {
+        console.warn(...this._createMessage(msg));
+    }
+    error(...msg: unknown[]) {
+        console.error(...this._createMessage(msg));
+    }
+}
+
+const logModuleName = (module: AnyModule) =>
+    `\u001b[1;32m${module.name.replace(/([A-Z])/g, ' $1').toUpperCase()}\x1b[0m`;
+
+const logger = new ConsoleLogger('MODULES');
+
+// TODO - create a class for initializing
+const _initializeModules = async <TModules extends Array<AnyModule>>(
+    modules: TModules,
+    config: ModulesConfigType<TModules>
+) => {
+    const instance$ = new BehaviorSubject<ModulesInstanceType<TModules>>(
+        {} as ModulesInstanceType<TModules>
+    );
+
+    from(modules)
+        .pipe(
+            delayWhen((module) => {
+                const hasDeps = !!module.deps;
+                if (hasDeps) {
+                    logger.debug(
+                        `module ${logModuleName(module)} requires dependencies`,
+                        module.deps
+                    );
+                    return instance$.pipe(
+                        filter(
+                            (instance) =>
+                                /** check that all dependencies are created */
+                                !!module.deps?.every((dep) =>
+                                    Object.keys(instance).includes(String(dep))
+                                )
+                        )
+                    );
+                }
+                return of(0);
+            }),
+            mergeMap((module) =>
+                /** assign module to modules object */
+                from(Promise.resolve(module.initialize(config, instance$.value))).pipe(
+                    map((instance) => {
+                        logger.debug(`initialized ${logModuleName(module)}`);
+                        return [module.name, instance];
+                    })
+                )
+            )
+        )
+        .subscribe({
+            next: ([name, module]) => {
+                instance$.next(Object.assign(instance$.value, { [name]: module }));
+            },
+            complete: () => instance$.complete(),
+        });
+
+    return lastValueFrom(instance$);
+};
 
 /**
  * Create an instances of provided instances
@@ -22,11 +108,19 @@ export const initializeModules = async <TModules extends Array<AnyModule>, TInst
     const afterConfiguration: Array<(config: ModulesConfigType<TModules>) => void> = [];
     const afterInit: Array<(instance: ModulesInstanceType<TModules>) => void> = [];
 
+    logger.debug(
+        `🛠 initializing ${!ref ? 'modules' : 'sub-modules'} ${modules.map(logModuleName)}`,
+        modules,
+        ref
+    );
+
     /** initialize config providers for all modules */
     const config: ModulesConfig<TModules> = await Object.values(modules).reduce(
         async (acc, module) => {
+            logger.debug(`configuring ${logModuleName(module)}`);
             const obj = await acc;
             const res = await Promise.resolve(module.configure?.(ref));
+            logger.debug(`configured ${logModuleName(module)}`);
             return Object.assign(obj, { [module.name]: res });
         },
         Promise.resolve({
@@ -42,6 +136,8 @@ export const initializeModules = async <TModules extends Array<AnyModule>, TInst
     /** protected config instance */
     Object.seal(config);
 
+    logger.debug(`✅ Configured ${modules.map(logModuleName)}`, config);
+
     /** allow callback to configure */
     configure && (await configure(config as any, ref));
 
@@ -52,23 +148,20 @@ export const initializeModules = async <TModules extends Array<AnyModule>, TInst
         )
     );
 
-    /** call module initializers */
-    const instance = await modules.reduce(async (acc, module) => {
-        const obj = await acc;
-        const res = await Promise.resolve(
-            module.initialize(config as unknown as ModulesConfigType<TModules>, obj)
-        );
-        return Object.assign(obj, { [module.name]: res });
-    }, Promise.resolve({} as ModulesInstanceType<TModules>));
+    const instance = await _initializeModules(modules, config);
+    logger.debug('✅ initialized');
 
     /** Protected instances */
     Object.seal(ref);
 
     /** call all added post config hooks  */
-    await Promise.all(
-        [...modules.map((x) => x.postInitialize), ...afterInit].map((x) =>
-            Promise.resolve(x?.(instance))
-        )
+    const postInitialize = [...modules.map((x) => x.postInitialize), ...afterInit];
+    await Promise.all(postInitialize.map((x) => Promise.resolve(x?.(instance))));
+    logger.debug('✅ post initialized');
+
+    logger.info(
+        `🚀 ${!ref ? 'modules' : 'sub-modules'} ready`,
+        process.env.NODE_ENV === 'development' && instance
     );
 
     return instance;
