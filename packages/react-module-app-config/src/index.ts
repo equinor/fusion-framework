e0c45d51cc573466cabd10f490a7d1612d6d6fdf
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-08-08 08:53:41 +0200

    feat(react-module-app-config): initial
    + create hook for fetching app config

---
+++ packages/react-module-app-config/src/index.ts
@@ -0,0 +1,4 @@
+export { appConfigModule, AppConfigModule } from '@equinor/fusion-framework-module-app-config';
+export { useAppConfig } from './useAppConfig';
+
+export { default } from '@equinor/fusion-framework-module-app-config';
