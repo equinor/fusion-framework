94e4d6bbb7fef010cd72e49242d68ed155592e11
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-09-12 12:10:38 +0200

    feat(module-http): expose simple config

--- packages/module-http/src/module.ts
+++ packages/module-http/src/module.ts
@@ -1,8 +1,12 @@
 import { HttpClientMsal } from './client-msal';
-import { IHttpClientConfigurator, HttpClientConfigurator } from './configurator';
+import { IHttpClientConfigurator, HttpClientConfigurator, HttpClientOptions } from './configurator';
 import { IHttpClientProvider, HttpClientProvider } from './provider';
 
-import type { Module, ModulesConfigType } from '@equinor/fusion-framework-module';
+import type {
+    Module,
+    ModuleConfigType,
+    IModuleConfigurator,
+} from '@equinor/fusion-framework-module';
 
 export type HttpModule = Module<'http', IHttpClientProvider, IHttpClientConfigurator>;
 
@@ -21,12 +25,22 @@ export const module: HttpModule = {
     initialize: ({ config }): HttpClientProvider => new HttpClientProvider(config),
 };
 
-export const setupHttpModule = (
-    config: ModulesConfigType<[HttpModule]>,
-    callback: (config: IHttpClientConfigurator) => void
-): void | Promise<void> => {
-    callback(config.http);
-};
+export const configureHttp = (
+    configure: (config: ModuleConfigType<HttpModule>, ref?: any) => void
+): IModuleConfigurator<HttpModule> => ({
+    module,
+    configure,
+});
+
+export const configureHttpClient = (
+    name: string,
+    args: HttpClientOptions
+): IModuleConfigurator<HttpModule> => ({
+    module,
+    configure: (config: ModuleConfigType<HttpModule>) => {
+        config.configureClient(name, args);
+    },
+});
 
 declare module '@equinor/fusion-framework-module' {
     interface Modules {
