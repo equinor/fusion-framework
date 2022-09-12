c88574a61d368841dd648c511d80cad2e5efd7c6
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-09-12 14:28:24 +0200

    fix(module): expose logger

--- packages/module/src/index.ts
+++ packages/module/src/index.ts
@@ -1,5 +1,7 @@
 export * from './types';
 export { initializeModules } from './initialize-modules';
 
+export { ModuleConsoleLogger } from './logger';
+
 export { ModulesConfigurator } from './configurator';
 export type { IModuleConfigurator, IModulesConfigurator } from './configurator';
