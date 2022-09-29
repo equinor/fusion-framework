3cd5f5c5f2548f8783a6cb7fd34dfb800c490b7b
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-09-29 12:22:09 +0200

    fix(react-app): update typing of module instance

--- packages/react-app/src/useAppModules.ts
+++ packages/react-app/src/useAppModules.ts
@@ -1,6 +1,9 @@
 import type { AppModulesInstance } from '@equinor/fusion-framework-app';
+import type { AnyModule } from '@equinor/fusion-framework-module';
 import { useModules } from '@equinor/fusion-framework-react-module';
 
-export const useAppModules = useModules<AppModulesInstance>;
+export const useAppModules = <
+    T extends Array<AnyModule> | unknown = unknown
+>(): AppModulesInstance<T> => useModules<AppModulesInstance<T>>();
 
 export default useAppModules;
