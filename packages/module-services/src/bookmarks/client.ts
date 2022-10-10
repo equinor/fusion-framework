216d54a7db580bb16249004778ecc9a5b8e8b242
Gustav-Eikaas <guei@equinor.com>
2022-10-10 15:31:31 +0200

    fix tsdoc

--- packages/module-services/src/bookmarks/client.ts
+++ packages/module-services/src/bookmarks/client.ts
@@ -1,6 +1,6 @@
 import { IHttpClient } from '@equinor/fusion-framework-module-http';
 
-import { ClientMethod, ApiVersion } from '@equinor/fusion-framework-module-services/context';
+import { ClientMethod } from '@equinor/fusion-framework-module-services/context';
 import getBookmark from './get/client';
 import { ApiVersions, GetBookmarkResult, GetBookmarksFn, GetBookmarksResult } from './get/types';
 
@@ -9,14 +9,10 @@ export class BookmarksApiClient<
     TClient extends IHttpClient = IHttpClient,
     TPayload = unknown
 > {
-    get Version(): typeof ApiVersion {
-        return ApiVersion;
-    }
-
     constructor(protected _client: TClient, protected _method: TMethod) {}
 
     /**
-     * Fetch context by id
+     * Fetch bookmark by id
      * @see {@link get/client}
      */
     public get<TVersion extends ApiVersions, TResult = GetBookmarkResult<TVersion, TPayload>>(
