89f4d38ba469edce5d31f3aefc2a7a4ef2c6353e
Gustav-Eikaas <guei@equinor.com>
2022-10-11 15:39:31 +0200

    add docs

--- vue-press/src/.vuepress/sidebar.ts
+++ vue-press/src/.vuepress/sidebar.ts
@@ -1,48 +1,52 @@
-import { sidebar } from "vuepress-theme-hope";
+import { sidebar } from 'vuepress-theme-hope';
 
 export default sidebar({
-  '/guide/': [
-    {
-      text: 'App',
-      prefix: '/guide/',
-      children: [
-        'app/',
-        'app/getting-started',
-        'app/app-modules',
-        'app/add-modules',
-        'app/legacy',
-      ]
-    },
-  ],
-  '/modules/': [
-    {
-      text: 'Modules',
-      prefix: '/modules/',
-      children: [
-        '',
-        'http/',
-        'event/',
-        'ag-grid/',
+    '/guide/': [
         {
-          text: 'Service Discovery',
-          link: 'service-discovery/',
+            text: 'App',
+            prefix: '/guide/',
+            children: [
+                'app/',
+                'app/getting-started',
+                'app/app-modules',
+                'app/add-modules',
+                'app/legacy',
+            ],
         },
+    ],
+    '/modules/': [
         {
-          text: 'Services',
-          prefix: 'services/',
-          link: 'services/',
-          children: [
-            {
-              text: 'module',
-              link: 'README.md',
-            },
-            {
-              text: 'context',
-              link: 'context.md'
-            }
-          ]
-        }
-      ]
-    }
-  ]
+            text: 'Modules',
+            prefix: '/modules/',
+            children: [
+                '',
+                'http/',
+                'event/',
+                'ag-grid/',
+                {
+                    text: 'Service Discovery',
+                    link: 'service-discovery/',
+                },
+                {
+                    text: 'Services',
+                    prefix: 'services/',
+                    link: 'services/',
+                    children: [
+                        {
+                            text: 'module',
+                            link: 'README.md',
+                        },
+                        {
+                            text: 'context',
+                            link: 'context.md',
+                        },
+                        {
+                            text: 'bookmarks',
+                            link: 'bookmarks.md',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
 });
