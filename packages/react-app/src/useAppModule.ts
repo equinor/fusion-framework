ffb66e3f488bf9c28870824b4d42748e5d072364
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-10-21 15:37:27 +0200

    fix(react-app): improve app module hook

--- packages/react-app/src/useAppModule.ts
+++ packages/react-app/src/useAppModule.ts
@@ -1,8 +1,17 @@
-import type { AppModulesInstance } from '@equinor/fusion-framework-app';
+import type { AppModules, AppModulesInstance } from '@equinor/fusion-framework-app';
+import { AnyModule, ModuleKey, ModuleType, ModuleTypes } from '@equinor/fusion-framework-module';
 import { useAppModules } from './useAppModules';
 
-export const useAppModule = <TKey extends keyof AppModulesInstance>(
+export const useAppModule = <
+    TType extends AnyModule | unknown = unknown,
+    TKey extends string = ModuleKey<ModuleTypes<AppModules<[TType]>>>
+>(
     module: TKey
-): AppModulesInstance[TKey] => useAppModules()[module];
+): TType extends AnyModule
+    ? ModuleType<TType>
+    : AppModulesInstance[Extract<keyof AppModulesInstance, TKey>] =>
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    useAppModules()[module];
 
 export default useAppModule;
