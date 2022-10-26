b90a81d955807ce33c37895edf8f2cbded310180
Gustav-Eikaas <guei@equinor.com>
2022-10-26 10:32:19 +0200

    feat: :sparkles: return undefined on status code 204

--- packages/module-http/src/lib/selectors/json-selector.ts
+++ packages/module-http/src/lib/selectors/json-selector.ts
@@ -4,10 +4,15 @@ export const jsonSelector = <TType = unknown, TResponse extends Response = Respo
     if (!response.ok) {
         throw new Error('Network response was not OK');
     }
+    //Status code 204 is no content
+    if (response.status === 204) {
+        return Promise.resolve() as Promise<TType>;
+    }
+
     try {
         return response.json();
     } catch (err) {
-        throw Error('failed to parse response');
+        return Promise.resolve(undefined) as Promise<TType>;
     }
 };
 
