c7a805fe8ed06396d2d11bbfcebbe2b504e9f69d
Gustav-Eikaas <guei@equinor.com>
2022-10-28 09:40:26 +0200

    refactor: :recycle: imports/exports

---
+++ packages/module-services/src/bookmarks/delete/index.ts
@@ -0,0 +1,6 @@
+export * from './types';
+
+export { deleteBookmark, default } from './client';
+
+export { generateEndpoint } from './generate-endpoint';
+export { generateParameters } from './generate-parameters';
