44e9016f410a5d99ba3667ca6f9c36dca7d51860
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-10-31 13:26:42 +0100

    style: fix lint

--- packages/module-app-config/src/module.ts
+++ packages/module-app-config/src/module.ts
@@ -59,7 +59,7 @@ export const module: AppConfigModule = {
         return configurator;
     },
     initialize: async ({ config, requireInstance }): Promise<IAppConfigProvider> => {
-        console.warn('module deprecated!, use module-app')
+        console.warn('module deprecated!, use module-app');
         /** if no client provided from config tyy to create one */
         if (!config.httpClient) {
             /** load http module */
