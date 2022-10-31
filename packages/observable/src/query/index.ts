90608c15c376e3d792019d3f442ac6e9e1a9996b
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-10-31 13:04:11 +0100

    feat(observable): create query value selector

--- packages/observable/src/query/index.ts
+++ packages/observable/src/query/index.ts
@@ -1,1 +1,8 @@
+import { map, Observable } from 'rxjs';
+import Query from './Query';
+
 export { default, Query, QueryCtorOptions, QueryOptions } from './Query';
+
+export const queryValue = <TType, TArgs>(
+    stream: ReturnType<Query<TType, TArgs>['query']>
+): Observable<TType> => stream.pipe(map((entry) => entry.value));
