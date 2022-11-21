99d0b37a676f485646d5a55e872efeba53e48233
Øyvind Eikeland <oyvind@eikeland.me>
2022-11-21 15:40:11 +0100

    feat(contextselector): set context type in app-react-context

--- cookbooks/app-react-context/src/config.ts
+++ cookbooks/app-react-context/src/config.ts
@@ -2,7 +2,13 @@ import type { AppModuleInitiator } from '@equinor/fusion-framework-app';
 import { enableContext } from '@equinor/fusion-framework-react-module-context';
 
 export const configure: AppModuleInitiator = (configurator) => {
-    enableContext(configurator);
+    enableContext(configurator, {
+        contextType: ['orgchart'], // set contextType to match against
+        /* FIlter result against regex */
+        // contextFilter: (result) => {
+        //     return result.filter((x) => x.title?.match(/ohan/));
+        // },
+    });
 };
 
 export default configure;
