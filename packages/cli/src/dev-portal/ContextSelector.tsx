bf8363e86909407632caa5ec46182643cbdd2205
Øyvind Eikeland <oyvind@eikeland.me>
2022-11-29 11:58:12 +0100

    fix(cli): contextselector improvements and comments

--- packages/cli/src/dev-portal/ContextSelector.tsx
+++ packages/cli/src/dev-portal/ContextSelector.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useFramework } from '@equinor/fusion-framework-react';
 import { ContextItem, IContextProvider } from '@equinor/fusion-framework-module-context';
+import { useObservableState } from '@equinor/fusion-observable/react';
 import '@equinor/fusion-framework-app';
 
 import {
@@ -8,10 +9,17 @@ import {
     ContextResult,
     ContextResultItem,
     ContextResolver,
+    ContextSelectEvent,
 } from '@equinor/fusion-react-context-selector';
 
-const mapper = (src: ContextItem<Record<string, unknown>>[]): ContextResult => {
-    const dst = src.map((i) => {
+/**
+ * Map context query result to ContextSelectorResult.
+ * Add any icons to selected types by using the 'graphic' property
+ * @param src context query result
+ * @returns src mapped to ContextResult type
+ */
+const mapper = (src: Array<ContextItem>): ContextResult => {
+    return src.map((i) => {
         return {
             id: i.id,
             title: i.title,
@@ -19,31 +27,59 @@ const mapper = (src: ContextItem<Record<string, unknown>>[]): ContextResult => {
             graphic: i.type.id === 'OrgChart' ? 'list' : undefined,
         };
     });
-
-    return dst;
 };
 
+/**
+ * Create a single ContextResultItem
+ * @param props pops for the item to merge with defaults
+ * @returns ContextResultItem
+ */
 const singleItem = (props: Partial<ContextResultItem>): ContextResultItem => {
     return Object.assign({ id: 'no-such-item', title: 'Change me' }, props);
 };
 
-const useQueryContext = (): ContextResolver => {
+/**
+ * Hook for querying context and setting resolver for ContextSelector component
+ * See React Components storybook for info about ContextSelector component and its resolver
+ * @link https://equinor.github.io/fusion-react-components/?path=/docs/data-contextselector--component
+ * @return Array<ContextResolver, SetContextCallback>
+ */
+const useQueryContext = (): [ContextResolver, (e: ContextSelectEvent) => void] => {
+    /* Framework modules */
     const framework = useFramework();
+
+    /* Current context observable */
+    const currentContext = useObservableState(framework.modules.context.currentContext$);
+
+    /* Set currentContext as initialResult in dropdown  */
+    let preselected: ContextResult = [];
+    if (currentContext) {
+        preselected = mapper([currentContext]);
+    }
+
+    /* context provider state */
     const [provider, setProvider] = useState<IContextProvider | undefined>();
+
+    /**
+     * Set context provider state if this app triggered the event.
+     * and only if the app has a context
+     * */
     useEffect(() => {
         return framework.modules.event.addEventListener('onAppModulesLoaded', (e) => {
-            if (e.detail) {
-                if (e.detail.env.manifest?.key === framework.modules.app.current?.key) {
-                    if (e.detail.modules.context) {
-                        setProvider(e.detail.modules.context);
-                    }
+            if (e.detail?.env.manifest?.key === framework.modules.app.current?.key) {
+                if (e.detail.modules.context) {
+                    setProvider(e.detail.modules.context);
                 }
             }
         });
     }, [framework]);
 
+    /**
+     * set resolver for ContextSelector
+     * @return ContextResolver
+     */
     const resolver = useMemo(
-        () => ({
+        (): ContextResolver => ({
             searchQuery: async (search: string) => {
                 if (!provider) {
                     return [];
@@ -71,43 +107,46 @@ const useQueryContext = (): ContextResolver => {
                         }),
                     ];
                 } catch (e) {
-                    console.error('ContextResolver:', e);
+                    console.log('ContextResolver query was cancelled');
+                    return [];
                 }
             },
-            initialResult: [
-                {
-                    id: '0',
-                    title: 'InitialResults',
-                    subTitle: 'Favourites or readily available contexts',
-                    isDisabled: true,
-                },
-                {
-                    id: '1',
-                    title: 'Another initial result',
-                    subTitle: 'Favourites or readily available contexts',
-                    isDisabled: true,
-                },
-            ],
+            initialResult: preselected,
         }),
-        [provider]
+        [provider, preselected]
     );
 
-    return resolver as ContextResolver;
+    /* Callback for setting current context to selected item id. */
+    const setContext = useCallback(
+        (e: ContextSelectEvent): void => {
+            if (e.nativeEvent.detail?.selected.length) {
+                framework.modules.context.contextClient.setCurrentContext(
+                    e.nativeEvent.detail.selected[0].id
+                );
+            }
+        },
+        [framework]
+    );
+
+    return [resolver, setContext];
 };
 
+/**
+ * Variants available. See component storybook for available attributes
+ * @link https://equinor.github.io/fusion-react-components/?path=/docs/data-contextselector--component
+ * @returns JSX element
+ */
 export const ContextSelector = () => {
-    const resolver = useQueryContext();
-
+    const [resolver, setContext] = useQueryContext();
     return (
         <ContextSelectorComponent
             id="context-selector-header"
-            label="Context Selector"
-            placeholder="Search for context..."
-            initialText="Start typing to search..."
+            placeholder="Search for context"
+            initialText="Start typing to search"
             dropdownHeight="300px"
             variant="header"
             resolver={resolver}
-            onSelect={(e) => console.log(e)}
+            onSelect={setContext}
         />
     );
 };
--- packages/cli/src/dev-portal/Header.tsx
+++ packages/cli/src/dev-portal/Header.tsx
@@ -1,6 +1,9 @@
-import ContextSelector from './ContextSelector';
-
+import { useState, useRef, useCallback } from 'react';
 import { makeStyles, createStyles, clsx, ThemeProvider, theme } from '@equinor/fusion-react-styles';
+import ContextSelector from './ContextSelector';
+import FusionLogo from './FusionLogo';
+import Button from '@equinor/fusion-react-button';
+import Icon from '@equinor/fusion-react-icon';
 
 /* typescript reference for makeStyles */
 import '@material-ui/styles';
@@ -8,7 +11,7 @@ import '@material-ui/styles';
 const useStyles = makeStyles(
     createStyles({
         header: {
-            padding: '1em',
+            padding: '0 1em',
         },
         center: {
             display: 'flex',
@@ -26,72 +29,24 @@ const useStyles = makeStyles(
 
 export const Header = () => {
     const styles = useStyles();
+    const buttonRef = useRef(null);
+    const [open, setOpen] = useState(false);
+
+    const menuToogle = useCallback(() => {
+        setOpen(!open);
+    }, [setOpen]);
+
     return (
         <ThemeProvider theme={theme}>
             <header className={clsx(styles.header, styles.center)}>
-                <div className="fc--Header__fusionTitleContainer--HvEMO">
-                    <a aria-current="page" className="active" href="/">
-                        <span className="fc--Header__fusionLogo--1bmhI">
-                            <svg
-                                width="50"
-                                height="35"
-                                viewBox="0 0 50 35"
-                                fill="none"
-                                style={{ transform: 'scale(0.7)' }}
-                            >
-                                <path
-                                    d="M0 2V23.1776L7.05405 16.1235V7.05405H16.1235L23.1776 0H2C0.895431 0 0 0.89543 0 2Z"
-                                    transform="translate(50 17.5) scale(0.92727 1.06779) rotate(135)"
-                                    fill="url(#paint0_linear)"
-                                ></path>
-                                <path
-                                    d="M0 2V23.1776L7.05405 16.1235V7.05405H16.1235L23.1776 0H2C0.895431 0 0 0.89543 0 2Z"
-                                    transform="translate(0 17.5) scale(0.92727 1.06779) rotate(-45)"
-                                    fill="url(#paint1_linear)"
-                                ></path>
-                                <path
-                                    d="M9.61965 36.6972L2.60087 29.6784L1.96135 22.3809L8.42623 22.9069L9.61965 36.6972Z"
-                                    transform="translate(33.8887 34.9863) scale(0.92727 -1.06779) rotate(45)"
-                                    fill="#990025"
-                                ></path>
-                                <path
-                                    d="M7.05434 7.05434L0 0L1.21096 13.8183L7.68846 14.3818L7.05434 7.05434Z"
-                                    transform="translate(33.8887 34.9863) scale(0.92727 -1.06779) rotate(45)"
-                                    fill="#990025"
-                                ></path>
-                                <path
-                                    d="M0 0L2.49398 29.5715L9.61965 36.6972L7.01878 7.01878L0 0Z"
-                                    transform="translate(33.8887 0.015625) scale(0.92727 1.06779) rotate(45)"
-                                    fill="#FF1243"
-                                ></path>
-                                <defs>
-                                    <linearGradient
-                                        id="paint0_linear"
-                                        x2="1"
-                                        gradientUnits="userSpaceOnUse"
-                                        gradientTransform="matrix(-13.5478 9.01983 -12.9578 -13.5478 18.0677 6.77391)"
-                                    >
-                                        <stop offset="0.508287" stop-color="#DC002E"></stop>
-                                        <stop offset="0.508387" stop-color="#FF1243"></stop>
-                                    </linearGradient>
-                                    <linearGradient
-                                        id="paint1_linear"
-                                        x2="1"
-                                        gradientUnits="userSpaceOnUse"
-                                        gradientTransform="matrix(-13.5478 9.01983 -12.9578 -13.5478 18.0677 6.77391)"
-                                    >
-                                        <stop offset="0.508287" stop-color="#DC002E"></stop>
-                                        <stop offset="0.508387" stop-color="#FF1243"></stop>
-                                    </linearGradient>
-                                </defs>
-                            </svg>
-                        </span>
-                    </a>
-                </div>
+                <Button ref={buttonRef} onClick={menuToogle} variant="ghost">
+                    <Icon icon="menu" />
+                </Button>
+                <FusionLogo scale={0.7} />
                 <div className={clsx(styles.contextselector)}>
                     <ContextSelector />
                 </div>
-            </header>
+            </header> 
         </ThemeProvider>
     );
 };
