c75726c62cc58c98b9f31258ce546bb3aaffb5ee
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-01 16:23:07 +0100

    feat(observable): add functionality for create state
    function should return:
    - created actions
    - dispatcher of actions
    - subject which holds state

---
+++ utils/observable/src/create-state.ts
@@ -0,0 +1,33 @@
+import { actionMapper } from './action-mapper';
+import { ActionReducerMapBuilder, createReducer } from './create-reducer';
+import { FlowSubject } from './FlowSubject';
+import { ActionDefinitions, ActionTypes } from './types/actions';
+import { ReducerWithInitialState } from './types/reducers';
+
+export function createState<TState, TActions extends ActionDefinitions>(
+    actions: TActions,
+    reducer_or_builder:
+        | ReducerWithInitialState<TState>
+        | {
+              builder: (builder: ActionReducerMapBuilder<TState>, actions: TActions) => void;
+              initial: TState | (() => TState);
+          }
+): {
+    subject: FlowSubject<TState, ActionTypes<TActions>>;
+    actions: TActions;
+    dispatch: ReturnType<typeof actionMapper<TActions>>;
+} {
+    const reducer =
+        typeof reducer_or_builder === 'function'
+            ? reducer_or_builder
+            : createReducer(reducer_or_builder.initial as TState, (builder) =>
+                  reducer_or_builder.builder(builder, actions)
+              );
+
+    const subject = new FlowSubject<TState, ActionTypes<TActions>>(reducer);
+    const dispatch = actionMapper(actions, subject);
+
+    return { actions, dispatch, subject };
+}
+
+export default createState;
