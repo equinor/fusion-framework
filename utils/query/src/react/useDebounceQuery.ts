d440f1940c19717bc7adf0da405454af87eda541
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-05 10:19:00 +0100

    fix(query): bind query method in hook

--- utils/query/src/react/useDebounceQuery.ts
+++ utils/query/src/react/useDebounceQuery.ts
@@ -6,11 +6,15 @@ export const useDebounceQuery = <TType, TArgs>(
     clientOrClientCtor: Query<TType, TArgs> | QueryCtorOptions<TType, TArgs>,
     options: UseDebounceOptions<[TArgs]>
 ) => {
-    const [client] = useState<Query<TType, TArgs>>(() =>
-        clientOrClientCtor instanceof Query ? clientOrClientCtor : new Query(clientOrClientCtor)
-    );
+    const [query] = useState<Query<TType, TArgs>['query']>(() => {
+        const client =
+            clientOrClientCtor instanceof Query
+                ? clientOrClientCtor
+                : new Query(clientOrClientCtor);
+        return client.query.bind(client);
+    });
 
-    return useDebounce(client.query.bind(client), options);
+    return useDebounce(query, options);
 };
 
 export default useDebounceQuery;
