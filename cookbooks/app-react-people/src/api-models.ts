97a448cec067c48265f5cdc1ef17df411d6d5319
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-05 09:55:18 +0100

    feat(cookbook): create a app for searching for people

---
+++ cookbooks/app-react-people/src/api-models.ts
@@ -0,0 +1,64 @@
+export type Project = {
+    id: string;
+    name: string;
+    domainId: string | null;
+    projectMasterId?: string | null;
+};
+
+export type Contract = {
+    id: string;
+    name: string;
+    number: string;
+};
+
+export type Position = {
+    name: string;
+    appliesFrom: Date;
+    appliesTo: Date;
+    isActive: boolean;
+    obs?: string | null;
+    workPack?: string | null;
+    positionExternalId?: string;
+    locationName?: string | null;
+    project: Project;
+    contract?: Contract | null | undefined;
+};
+
+export type PersonDocument = {
+    azureUniqueId: string;
+    mail: string;
+    name: string;
+    jobTitle?: any;
+    department?: any;
+    mobilePhone?: any;
+    officeLocation?: any;
+    upn: string;
+    accountType: string;
+    isExpired: boolean;
+    positions: Position[];
+};
+
+export type PersonResult = {
+    document: PersonDocument;
+    '@search.score': number;
+};
+
+export type PersonSearchResult = {
+    results: PersonResult[];
+    continuationToken?: ContinuationToken | null;
+    count: number;
+};
+
+export type SearchNextPageParameters = {
+    search: string;
+    searchFields?: any;
+    filter?: string;
+    orderBy?: any;
+    top?: any;
+    skip: number;
+    includeTotalResultCount: boolean;
+};
+
+export type ContinuationToken = {
+    '@search.nextPageParameters': SearchNextPageParameters;
+};
