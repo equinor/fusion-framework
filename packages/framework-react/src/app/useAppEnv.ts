2faac446374ad89e10d683488b27aba185dcc74f
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-09 13:40:46 +0100

    fix(framework-react): update hooks
    + rename `useApp` to `useAppManifest`
    + rename `useApps` to `useAppManifests`
    + update return type of `useCurrentApp`
    
    BREAKING CHANGES: `useCurrentApp` now return `App` instance

--- packages/framework-react/src/app/useAppEnv.ts
+++ packages/framework-react/src/app/useAppEnv.ts
@@ -1,11 +1,11 @@
-import { useApp } from './useApp';
+import { useAppManifest } from './useAppManifest';
 import { useAppConfig } from './useAppConfig';
 
 export const useAppEnv = (appKey: string) => {
-    const { value: manifest, isLoading: manifestLoading } = useApp(appKey);
+    const { value: manifest, isLoading: manifestLoading } = useAppManifest(appKey);
     const { value: config, isLoading: configLoading } = useAppConfig({ appKey });
 
     return { manifest, config, isLoading: manifestLoading || configLoading };
 };
 
-export default useApp;
+export default useAppEnv;
--- packages/framework-react/src/app/useCurrentApp.ts
+++ packages/framework-react/src/app/useCurrentApp.ts
@@ -1,9 +1,13 @@
+import { App } from '@equinor/fusion-framework-module-app';
 import { useObservableState } from '@equinor/fusion-observable/react';
 import { useCallback } from 'react';
 
 import { useFramework } from '../useFramework';
 
-export const useCurrentApp = () => {
+export const useCurrentApp = (): {
+    currentApp: App | undefined;
+    setCurrentApp: (appKey: string) => void;
+} => {
     const provider = useFramework().modules.app;
     const currentApp = useObservableState(provider.current$);
     const setCurrentApp = useCallback(
