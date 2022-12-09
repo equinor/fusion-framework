d5cbd74b89cd9cba0dabef4a62f585c72e3c14be
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-09 13:21:24 +0100

    feat: create instance container for application
    + add action for app state
    + add state reducers
    + add flows for app
    + create instance container

---
+++ packages/module-app/src/app/actions.ts
@@ -0,0 +1,50 @@
+import {
+    ActionInstanceMap,
+    ActionTypes,
+    createAction,
+    createAsyncAction,
+} from '@equinor/fusion-observable';
+import { AppConfig, AppManifest, AppModulesInstance, AppScriptModule } from '../types';
+
+const createActions = () => ({
+    /** Manifest loading */
+    setManifest: createAction('set_manifest', (manifest: AppManifest) => ({
+        payload: manifest,
+        meta: { created: Date.now() },
+    })),
+    fetchManifest: createAsyncAction(
+        'fetch_manifest',
+        (key: string) => ({ payload: key }),
+        (manifest: AppManifest) => ({ payload: manifest }),
+        (error: unknown) => ({ payload: error })
+    ),
+    /** Config loading */
+    setConfig: createAction('set_config', (config: AppConfig) => ({ payload: config })),
+    fetchConfig: createAsyncAction(
+        'fetch_config',
+        (key: string) => ({ payload: key }),
+        (config: AppConfig) => ({ payload: config }),
+        (error: unknown) => ({ payload: error })
+    ),
+    /** App loading */
+    setModule: createAction('set_module', (module: any) => ({ payload: module })),
+    importApp: createAsyncAction(
+        'import_app',
+        (entrypoint: string) => ({ payload: entrypoint }),
+        (module: AppScriptModule) => ({ payload: module }),
+        (error: unknown) => ({ payload: error })
+    ),
+
+    // App Instance
+    setInstance: createAction('set_instance', (instance: AppModulesInstance) => ({
+        payload: instance,
+    }))
+});
+
+export const actions = createActions();
+
+export type ActionBuilder = ReturnType<typeof createActions>;
+
+export type ActionMap = ActionInstanceMap<ActionBuilder>;
+
+export type Actions = ActionTypes<typeof actions>;
