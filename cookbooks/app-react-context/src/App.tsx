7120315750543e61e2291e4075b007310b8894fb
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-09 13:53:02 +0100

    fix(cookbooks/app-react-context): update context content

--- cookbooks/app-react-context/src/App.tsx
+++ cookbooks/app-react-context/src/App.tsx
@@ -1,7 +1,13 @@
+import { useModuleCurrentContext } from '@equinor/fusion-framework-react-module-context';
 import { StrictMode } from 'react';
 
 export const App = () => {
-    return <StrictMode></StrictMode>;
+    const { currentContext } = useModuleCurrentContext();
+    return (
+        <StrictMode>
+            <pre>{JSON.stringify(currentContext, null, 4)}</pre>
+        </StrictMode>
+    );
 };
 
 export default App;
--- cookbooks/app-react-context/src/config.ts
+++ cookbooks/app-react-context/src/config.ts
@@ -2,12 +2,8 @@ import type { AppModuleInitiator } from '@equinor/fusion-framework-app';
 import { enableContext } from '@equinor/fusion-framework-react-module-context';
 
 export const configure: AppModuleInitiator = (configurator) => {
-    enableContext(configurator, {
-        contextType: ['orgchart'], // set contextType to match against
-        /* FIlter result against regex */
-        // contextFilter: (result) => {
-        //     return result.filter((x) => x.title?.match(/ohan/));
-        // },
+    enableContext(configurator, (builder) => {
+        builder.setContextType(['orgchart']);
     });
 };
 
