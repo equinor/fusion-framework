4de3289942f8e3d81f4ee5749311479f4f49b680
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-09 13:41:30 +0100

    fix(utils/cli): update loading of application

--- packages/cli/src/dev-portal/AppLoader.tsx
+++ packages/cli/src/dev-portal/AppLoader.tsx
@@ -1,44 +1,86 @@
 import { useEffect, useRef, useState } from 'react';
 
+import { Subscription } from 'rxjs';
+
 import { useFramework } from '@equinor/fusion-framework-react';
 import { StarProgress } from '@equinor/fusion-react-progress-indicator';
+
+import { useObservableState } from '@equinor/fusion-observable/react';
+
+import { AppManifestError } from '@equinor/fusion-framework-module-app/errors.js';
 
 import { ErrorViewer } from './ErrorViewer';
 
 export const AppLoader = (props: { appKey: string }) => {
     const { appKey } = props;
     const fusion = useFramework();
-    const ref = useRef<HTMLSpanElement>(null);
+    const ref = useRef<HTMLElement>(null);
+    const appRef = useRef<HTMLDivElement>(document.createElement('div'));
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [appModule, setAppModule] = useState<any | undefined>();
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<Error | undefined>();
 
+    const currentApp = useObservableState(fusion.modules.app.current$);
+
     useEffect(() => {
-        setLoading(true);
-        const sub = fusion.modules.app.loadApp(appKey).subscribe({
-            next: (module) => setAppModule(module),
-            error: setError,
-            complete: () => {
-                setLoading(false);
-            },
-        });
-        return () => sub.unsubscribe();
+        fusion.modules.app.setCurrentApp(appKey);
     }, [appKey]);
 
     useEffect(() => {
-        if (appModule && ref.current) {
-            const { manifest, config, module } = appModule;
-            const [basename] = window.location.pathname.match(/\/?apps\/[a-z|-]+(\/)?/g) ?? [];
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            const renderFn = module.render ?? module.default;
-            return renderFn(ref.current, { fusion, env: { manifest, config, basename } });
+        setLoading(true);
+        setError(undefined);
+        const subscription = new Subscription();
+        subscription.add(
+            currentApp?.initialize().subscribe({
+                next: ([manifest, module, config]) => {
+                    const [basename] = window.location.pathname.match(
+                        /\/?apps\/[a-z|-]+(\/)?/g
+                    ) ?? [''];
+                    appRef.current = document.createElement('div');
+                    const render = module.renderApp ?? module.default;
+                    subscription.add(
+                        render(appRef.current, { fusion, env: { basename, config, manifest } })
+                    );
+                },
+                complete: () => {
+                    setLoading(false);
+                },
+                error: (err) => {
+                    setError(err);
+                },
+            })
+        );
+        return () => subscription?.unsubscribe();
+    }, [currentApp, ref.current]);
+
+    useEffect(() => {
+        const refEl = ref.current;
+        const appEl = appRef.current;
+        if (!(appEl && refEl)) {
+            return;
         }
-    }, [fusion, ref, appModule]);
+        refEl.appendChild(appEl);
+        return () => {
+            try {
+                refEl.removeChild(appEl);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+    }, [ref.current, appRef.current]);
+
+    console.log(ref.current, appRef.current);
 
     if (error) {
+        if (error.cause instanceof AppManifestError) {
+            return (
+                <div>
+                    <h2>🔥 Failed to load application manifest 🤬</h2>
+                    <h3>{error.cause.type}</h3>
+                    <ErrorViewer error={error} />;
+                </div>
+            );
+        }
         return (
             <div>
                 <h2>🔥 Failed to load application 🤬</h2>
@@ -47,12 +89,11 @@ export const AppLoader = (props: { appKey: string }) => {
         );
     }
 
-    return (
-        <div>
-            {loading && <StarProgress text="Loading Application" />}
-            {<span ref={ref} style={{ display: !appModule ? 'none' : '' }}></span>}
-        </div>
-    );
+    if (loading) {
+        return <StarProgress text="Loading Application" />;
+    }
+
+    return <section ref={ref}></section>;
 };
 
 export default AppLoader;
