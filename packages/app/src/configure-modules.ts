217580894ca95359ed6abf463293eea323b730b6
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-09 13:18:06 +0100

    refactor(app): move event and typing to module-app

--- packages/app/src/configure-modules.ts
+++ packages/app/src/configure-modules.ts
@@ -1,12 +1,15 @@
 import { Fusion } from '@equinor/fusion-framework';
 import type { AnyModule } from '@equinor/fusion-framework-module';
-import { FrameworkEvent, FrameworkEventInit } from '@equinor/fusion-framework-module-event';
 
 import { AppConfigurator } from './configurator';
 import type { AppModulesInstance, AppModuleInitiator, AppEnv } from './types';
 
 export const configureModules =
-    <TModules extends Array<AnyModule> | never, TRef extends Fusion = Fusion, TEnv = AppEnv>(
+    <
+        TModules extends Array<AnyModule> | never,
+        TRef extends Fusion = Fusion,
+        TEnv extends AppEnv = AppEnv
+    >(
         cb?: AppModuleInitiator<TModules, TRef, TEnv>
     ): ((args: { fusion: TRef; env: TEnv }) => Promise<AppModulesInstance<TModules>>) =>
     async (args: { fusion: TRef; env: TEnv }): Promise<AppModulesInstance<TModules>> => {
@@ -14,22 +17,13 @@ export const configureModules =
         if (cb) {
             await Promise.resolve(cb(configurator, args));
         }
-        const modules = await configurator.initialize(args.fusion.modules);
+        const modules = (await configurator.initialize(
+            args.fusion.modules
+        )) as AppModulesInstance<TModules>;
         modules.event.dispatchEvent('onAppModulesLoaded', {
-            detail: { modules, env: args.env },
+            detail: { appKey: args.env.manifest.appKey, modules },
         });
-        return modules as unknown as AppModulesInstance<TModules>;
+        return modules;
     };
 
 export default configureModules;
-
-declare module '@equinor/fusion-framework-module-event' {
-    interface FrameworkEventMap {
-        onAppModulesLoaded: FrameworkEvent<
-            FrameworkEventInit<{
-                modules: AppModulesInstance;
-                env: AppEnv;
-            }>
-        >;
-    }
-}
