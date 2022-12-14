af5586f563e32c1e7e577a3e6837be610a135fc9
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-14 10:29:00 +0100

    fix: remove deprecated hooks

--- packages/framework-react/src/app/useCurrentApp.ts
+++ packages/framework-react/src/app/useCurrentApp.ts
@@ -1,4 +1,4 @@
-import { App } from '@equinor/fusion-framework-module-app';
+import { App, AppModule } from '@equinor/fusion-framework-module-app';
 import { useObservableState } from '@equinor/fusion-observable/react';
 import { useCallback } from 'react';
 
@@ -8,12 +8,16 @@ export const useCurrentApp = (): {
     currentApp: App | undefined;
     setCurrentApp: (appKey: string) => void;
 } => {
-    const provider = useFramework().modules.app;
+    const provider = useFramework<[AppModule]>().modules.app;
+    if (!provider) {
+        throw Error('Current framework does not have AppModule configured');
+    }
     const currentApp = useObservableState(provider.current$);
     const setCurrentApp = useCallback(
         (appKey: string) => provider.setCurrentApp(appKey),
         [provider]
     );
+
     return {
         currentApp,
         setCurrentApp,
--- packages/framework-react/src/hooks/index.ts
+++ packages/framework-react/src/hooks/index.ts
@@ -1,3 +1,2 @@
 export { useCurrentUser } from './use-current-user';
 export { useHttpClient } from './use-http-client';
-export { useFramework } from './use-framework';
