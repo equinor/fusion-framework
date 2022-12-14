761dfb0d5675fba05bd31889d4bd4d2f721880ec
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-14 10:36:11 +0100

    fix(utils/observable): improve type-hinting

--- utils/observable/src/react/useObservableState.ts
+++ utils/observable/src/react/useObservableState.ts
@@ -3,6 +3,8 @@ import { BehaviorSubject } from 'rxjs';
 import { Observable } from '../types';
 import { useObservableLayoutSubscription } from './useObservableSubscription';
 
+export function useObservableState<S>(subject: Observable<S>):  S|undefined;
+
 /**
  * Hook for extracting state of observable.
  * **note** when state changes the consumer of the hook will rerender
@@ -11,11 +13,13 @@ import { useObservableLayoutSubscription } from './useObservableSubscription';
  * @param initial initial value
  * @returns current state of observable
  */
-export const useObservableState = <S>(subject: Observable<S>, initial?: S): S | undefined => {
+export function useObservableState<S>(subject: Observable<S>, initial: S): S;
+
+export function useObservableState<S>(subject: Observable<S>, initial?: S): S|undefined {
     initial ??= (subject as BehaviorSubject<S>).value;
     const [state, setState] = useState<S | undefined>(initial);
     useObservableLayoutSubscription(subject, setState);
-    return state;
+    return state as S;
 };
 
 export default useObservableState;
