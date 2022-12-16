fcce7aadede3f4365ece7febf3fe9cdbc7c62308
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-16 14:04:39 +0100

    fix(module-app): fix import

--- packages/module-app/src/enable-app-module.ts
+++ packages/module-app/src/enable-app-module.ts
@@ -1,5 +1,5 @@
 import type { IModulesConfigurator } from '@equinor/fusion-framework-module';
-import type { AppConfigBuilderCallback } from 'AppConfigBuilder';
+import type { AppConfigBuilderCallback } from './AppConfigBuilder';
 
 import { module } from './module';
 
