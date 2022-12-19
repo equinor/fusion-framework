210bb299db2ff1d3056566545c3b9271e08d8562
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-12-19 15:24:56 +0100

    fix(react-app): check if manifest is provided in env
    since legacy `@equinor/fusion-cli` not yet has `@equinor/fusion-framework-module-app` configured, manifest is not provided during init of application modules

--- packages/react-app/src/create-legacy-app.tsx
+++ packages/react-app/src/create-legacy-app.tsx
@@ -6,17 +6,25 @@ import type { AnyModule } from '@equinor/fusion-framework-module';
 import type { AppEnv, AppModuleInitiator } from '@equinor/fusion-framework-app';
 
 import { createComponent } from './create-component';
+import { AppModule } from '@equinor/fusion-framework-module-app';
 
 export const createLegacyApp = <TModules extends Array<AnyModule>>(
     Component: React.ComponentType,
     configure?: AppModuleInitiator<TModules>
 ) => {
     return (): JSX.Element => {
-        const fusion = useFramework();
+        const fusion = useFramework<[AppModule]>();
         const RenderComponent = useMemo(() => {
             const creator = createComponent(Component, configure);
+            // @eikeland
+            // TODO - recheck when legacy fusion-cli is updated!
+            const appProvider = fusion.modules.app;
+            if (appProvider?.current) {
+                const { config, manifest } = appProvider.current;
+                return creator(fusion, { config, manifest } as unknown as AppEnv);
+            }
             return creator(fusion, {} as unknown as AppEnv);
-        }, []);
+        }, [fusion]);
         return (
             <Suspense fallback={<p>loading app</p>}>
                 <RenderComponent />
