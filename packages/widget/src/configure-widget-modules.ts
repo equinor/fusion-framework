00c9849641e9347d44b10b1b3bd088073379147b
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-01-19 11:03:37 +0100

    feat(widget): add configurator for widget modules

---
+++ packages/widget/src/configure-widget-modules.ts
@@ -0,0 +1,59 @@
+import { Fusion } from '@equinor/fusion-framework';
+import type { AnyModule } from '@equinor/fusion-framework-module';
+
+import { WidgetConfigurator } from './WidgetConfigurator';
+import type { WidgetModulesInstance, WidgetModuleInitiator, WidgetEnv } from './types';
+
+/**
+ * 
+ * Creates a callback for initializing configuration of application modules
+ * 
+ * @example
+ ```ts
+    const initialize =  configureModules((configurator, args) => {
+        configurator.configure(someModule);
+    });
+    await initialize({ fusion, { manifest, config }});
+ ```
+ * @template TModules Addition modules
+ * @template TRef usually undefined, optional references
+ * @template TEnv environment object for configuring modules
+ * 
+ * @param cb configuration callback
+ * 
+ * @returns initialize function, executes configurator
+ */
+export const configureWidgetModules =
+    <
+        TModules extends Array<AnyModule> | never,
+        TRef extends Fusion = Fusion,
+        TEnv extends WidgetEnv = WidgetEnv
+    >(
+        cb?: WidgetModuleInitiator<TModules, TRef, TEnv>
+    ): ((args: { fusion: TRef; env: TEnv }) => Promise<WidgetModulesInstance<TModules>>) =>
+    /**
+     *
+     * Callback for initializing application modules
+     *
+     * @param args - Fusion and application  environments (manifest, config ...)
+     * @returns initialized app modules
+     */
+    async (args: { fusion: TRef; env: TEnv }): Promise<WidgetModulesInstance<TModules>> => {
+        const configurator = new WidgetConfigurator<TModules, TRef['modules']>();
+        if (cb) {
+            await Promise.resolve(cb(configurator, args));
+        }
+        const modules = (await configurator.initialize(
+            args.fusion.modules
+        )) as WidgetModulesInstance<TModules>;
+
+        // TODO - fire event when widget is loaded
+        // if (args.env.manifest?.key) {
+        //     modules.event.dispatchEvent('onAppModulesLoaded', {
+        //         detail: { appKey: args.env.manifest.key, modules },
+        //     });
+        // }
+        return modules;
+    };
+
+export default configureWidgetModules;
---
+++ packages/widget/src/types.ts
@@ -0,0 +1,61 @@
+import type { Fusion } from '@equinor/fusion-framework';
+
+import type { AnyModule } from '@equinor/fusion-framework-module';
+
+import type {
+    WidgetManifest,
+    WidgetModulesInstance,
+} from '@equinor/fusion-framework-module-widget';
+
+import type { IWidgetConfigurator } from './WidgetConfigurator';
+
+export type {
+    WidgetManifest,
+    WidgetModules,
+    WidgetModulesInstance,
+} from '@equinor/fusion-framework-module-widget';
+
+/**
+ * Application environment args
+ * Arguments provided when initializing/configuring application modules
+ *
+ * @template TConfig config value type
+ * @template TProps [__not in use__] properties for application component
+ */
+export type WidgetEnv<TProps = unknown> = {
+    manifest: WidgetManifest;
+    /** missing backend feature */
+    config?: null;
+    props?: TProps;
+};
+
+/**
+ * Blueprint for initializing application modules
+ *
+ * @template TModules Addition modules
+ * @template TRef usually undefined, optional references
+ * @template TEnv environment object for configuring modules
+ */
+export type WidgetModuleInitiator<
+    TModules extends Array<AnyModule> | unknown = unknown,
+    TRef extends Fusion = Fusion,
+    TEnv = WidgetEnv
+> = (
+    configurator: IWidgetConfigurator<TModules, TRef['modules']>,
+    args: { fusion: TRef; env: TEnv }
+) => void | Promise<void>;
+
+/**
+ * Blueprint for creating application initialization
+ *
+ * @template TModules Addition modules
+ * @template TRef usually undefined, optional references
+ * @template TEnv environment object for configuring modules
+ */
+export type WidgetModuleInit<
+    TModules extends Array<AnyModule> | unknown,
+    TRef extends Fusion = Fusion,
+    TEnv = WidgetEnv
+> = (
+    cb: WidgetModuleInitiator<TModules, TRef, TEnv>
+) => (args: { fusion: TRef; env: TEnv }) => Promise<WidgetModulesInstance<TModules>>;
