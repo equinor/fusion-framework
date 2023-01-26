543f9b594f89d7ff8a6763502bae6fe91e502547
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-01-26 11:10:31 +0100

    feat(framework-react): expose hook for signalr

---
+++ packages/framework-react/src/signalr/index.ts
@@ -0,0 +1,24 @@
+import { useFramework } from '../useFramework';
+
+import { SignalRModule, useProviderTopic } from '@equinor/fusion-framework-react-module-signalr';
+
+/**
+ * hook for subscribing to a topic of a SignalR hub
+ *
+ * @see {@link [module signalr](https://equinor.github.io/fusion-framework/modules/signalr)}
+ *
+ * @param hubId identifier of connection hub (must be configured)
+ * @param topicId identifier of topic to connect to
+ */
+export const useSignalR = <T>(
+    hubId: string,
+    topicId: string
+): ReturnType<typeof useProviderTopic<T>> => {
+    const provider = useFramework<[SignalRModule]>().modules.signalR;
+    if (!provider) {
+        throw Error(
+            'SignalR is not configured, see @equinor/fusion-framework-react-module-signalr'
+        );
+    }
+    return useProviderTopic(provider, hubId, topicId);
+};
