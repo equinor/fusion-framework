aa2f316e34d6877cfd0e7a71216e227d06b76b11
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-01-26 13:51:06 +0100

    refactor: rename hooks

--- packages/react-module-signalr/src/use-signalr.ts
+++ packages/react-module-signalr/src/use-signalr.ts
@@ -1,10 +1,11 @@
 import { useModule } from '@equinor/fusion-framework-react-module';
 import { moduleKey } from '@equinor/fusion-framework-module-signalr';
-import { useProviderTopic } from './use-provider-topic';
+import { useSignalRProvider } from './use-signalr-provider';
 
 export const useTopic = <T>(
     hubId: string,
     topicId: string
-): ReturnType<typeof useProviderTopic<T>> => useProviderTopic(useModule(moduleKey), hubId, topicId);
+): ReturnType<typeof useSignalRProvider<T>> =>
+    useSignalRProvider(useModule(moduleKey), hubId, topicId);
 
 export default useTopic;
