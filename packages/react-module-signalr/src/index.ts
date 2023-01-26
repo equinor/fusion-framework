4463fd876f00eafd7a1d242d3fbe97d55dbd913c
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-01-26 09:28:28 +0100

    feat(react-module-signalr): expose cjs module

--- packages/react-module-signalr/src/index.ts
+++ packages/react-module-signalr/src/index.ts
@@ -1,2 +1,10 @@
 export { useTopic } from './use-topic';
 export { useTopicValue } from './use-topic-value';
+
+export { Topic, enableSignalR } from '@equinor/fusion-framework-module-signalr';
+
+export type {
+    ISignalRConfigurator,
+    SignalRHubConfig,
+    SignalRModule,
+} from '@equinor/fusion-framework-module-signalr';
