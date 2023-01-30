97425af97ec20d1c2f85fd248935a6b385ea9ad2
Øyvind Eikeland <oyvind@eikeland.me>
2023-01-30 11:13:13 +0100

    docs(framework-react): tscomment on hook useAppProvider

--- packages/react/framework/src/app/useAppProvider.ts
+++ packages/react/framework/src/app/useAppProvider.ts
@@ -3,6 +3,10 @@ import { AppModule } from '@equinor/fusion-framework-module-app';
 
 import { useFramework } from '../useFramework';
 
+/**
+ * React Hook - get appProvider interface
+ * @returns framework AppModule
+ */
 export const useAppProvider = (): FusionModulesInstance<[AppModule]>['app'] => {
     const provider = useFramework<[AppModule]>().modules.app;
     if (!provider) {
