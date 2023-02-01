fd62fb35667730d6d1f915bab4fc0c9bd8f6152b
Øyvind Eikeland <oyvind@eikeland.me>
2023-02-01 13:56:42 +0100

    fix(framework-app): exporting hooks useApps and useAppProvider

--- packages/react/framework/src/app/index.ts
+++ packages/react/framework/src/app/index.ts
@@ -1,3 +1,5 @@
 export type { AppConfig, AppManifest, AppType, App } from '@equinor/fusion-framework-module-app';
 
 export { useCurrentApp } from './useCurrentApp';
+export { useApps } from './useApps';
+export { useAppProvider } from './useAppProvider';
