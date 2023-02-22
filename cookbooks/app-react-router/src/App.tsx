6cc3f723ac85104fe3b5b3ce642608e33c4e0abe
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-02-22 15:21:47 +0100

    fix(cookbooks/app-react-router): update router

--- cookbooks/app-react-router/src/App.tsx
+++ cookbooks/app-react-router/src/App.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react';
 import AppRouter from './Router';
 
 export default function () {
     return (
-        <StrictMode>
+        <>
             <h1>React Router App</h1>
             <AppRouter />
-        </StrictMode>
+        </>
     );
 }
--- cookbooks/app-react-router/src/Router.tsx
+++ cookbooks/app-react-router/src/Router.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useState } from 'react';
+
 import { RouterProvider } from 'react-router-dom';
 import { NavigationModule } from '@equinor/fusion-framework-module-navigation';
 import { useAppModule } from '@equinor/fusion-framework-react-app';
@@ -6,6 +8,6 @@ import routes from './Routes';
 
 export default function () {
     const module = useAppModule<NavigationModule>('navigation');
-    const router = module.createRouter(routes);
+    const [router] = useState(() => module.createRouter(routes));
     return <RouterProvider router={router} fallbackElement={<p>:(</p>} />;
 }
