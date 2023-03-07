69cbc0c6d6443b37b954a6f8d09da19472c59c62
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-03-07 14:11:45 +0100

    chore: yarn look file update

--- packages/react/modules/bookmark/src/portal/useBookmarkNavigate.ts
+++ packages/react/modules/bookmark/src/portal/useBookmarkNavigate.ts
@@ -1,30 +1,36 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useFramework } from '@equinor/fusion-framework-react';
 import { useNavigate } from 'react-router-dom';
+import { BookmarkModule } from '@equinor/fusion-framework-module-bookmark';
+
 // import { removeBookmarkIdFromURL } from '@equinor/fusion-framework-module-bookmark/utils';
 
-export const useBookmarkNavigate = (appPathname: (appKey: string) => string): void => {
-    const { event, context } = useFramework().modules;
+export const useBookmarkNavigate = (): void => {
+    const { event, context, bookmark } = useFramework<[BookmarkModule]>().modules;
 
     const navigate = useNavigate();
 
-    useEffect(() => {
-        event.addEventListener('onBookmarkChanged', (e) => {
+    useLayoutEffect(() => {
+        const sub = event.addEventListener('onBookmarkChanged', (e) => {
             const { appKey } = e.detail;
 
-            if (window.location.pathname !== appPathname(appKey)) {
-                const url = new URL(appPathname(appKey), window.location.origin);
+            const bookmarkPath = bookmark.config.appRoute(appKey);
+
+            if (window.location.pathname !== bookmarkPath) {
+                const url = new URL(bookmarkPath, window.location.origin);
                 url.search = window.location.search;
                 navigate(url.pathname + url.search);
             }
 
             if (e.detail.context) {
-                context.contextClient.setCurrentContext(e.detail.context.id);
+                context.contextClient.currentContext?.id !== e.detail.context.id &&
+                    context.contextClient.setCurrentContext(e.detail.context.id);
             }
 
             // removeBookmarkIdFromURL();
         });
-    }, [appPathname, context, event, navigate]);
+        return sub;
+    }, [bookmark.config, context, event, navigate]);
 };
 
 export default useBookmarkNavigate;
