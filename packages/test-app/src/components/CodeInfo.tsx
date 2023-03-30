b5f513b64a0e2ea282fc48d93be998de45ff36a0
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-03-30 14:10:45 +0200

    chore: delete test app

--- packages/test-app/src/components/CodeInfo.tsx
+++
@@ -1,7 +0,0 @@
-export const CodeInfo = (props: { data: unknown }) => (
-    <pre>
-        <code style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(props.data, undefined, 2)}</code>
-    </pre>
-);
-
-export default CodeInfo;
--- packages/test-app/src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { CodeInfo } from './CodeInfo';
