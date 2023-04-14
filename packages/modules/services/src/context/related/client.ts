909de590e366a26fa5f3620918e599dc70826596
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-04-14 17:24:58 +0200

    feat(module-services): add api interface for resolving related context

---
+++ packages/modules/services/src/context/related/client.ts
@@ -0,0 +1,35 @@
+import { ClientRequestInit, IHttpClient } from '@equinor/fusion-framework-module-http/client';
+
+import { ApiVersion } from '@equinor/fusion-framework-module-services/context';
+
+import { generateParameters } from './generate-parameters';
+
+import type {
+    RelatedContextArgs,
+    RelatedContextResponse,
+    RelatedContextResult,
+    ClientMethod,
+} from './types';
+
+/**
+ * Function for querying the context service
+ */
+export const relatedContexts =
+    <
+        TVersion extends string = keyof typeof ApiVersion,
+        TMethod extends keyof ClientMethod = keyof ClientMethod,
+        TClient extends IHttpClient = IHttpClient
+    >(
+        client: TClient,
+        version: TVersion,
+        method: TMethod = 'json' as TMethod
+    ) =>
+    <T = RelatedContextResponse<TVersion>>(
+        args: RelatedContextArgs<TVersion>,
+        init?: ClientRequestInit<TClient, T>
+    ): RelatedContextResult<TVersion, TMethod, T> =>
+        client[method](
+            ...generateParameters<T, TVersion, TClient>(version, args, init)
+        ) as RelatedContextResult<TVersion, TMethod, T>;
+
+export default relatedContexts;
---
+++ packages/modules/services/src/context/related/generate-parameters.ts
@@ -0,0 +1,23 @@
+import { ClientRequestInit, IHttpClient } from '@equinor/fusion-framework-module-http/client';
+
+import { ApiVersion } from '@equinor/fusion-framework-module-services/context';
+
+import { generateEndpoint } from './generate-endpoint';
+
+import type { RelatedContextArgs, ApiClientArguments } from './types';
+
+/** Function for generating parameter for querying context service  */
+export const generateParameters = <
+    TResult,
+    TVersion extends string = keyof typeof ApiVersion,
+    TClient extends IHttpClient = IHttpClient
+>(
+    version: TVersion,
+    args: RelatedContextArgs<TVersion>,
+    init?: ClientRequestInit<TClient, TResult>
+): ApiClientArguments<TClient, TResult> => {
+    const path = generateEndpoint(version, args);
+    return [path, init];
+};
+
+export default generateParameters;
