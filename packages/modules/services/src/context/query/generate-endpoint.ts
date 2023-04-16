0b60f016f789520dec1f78e6cc29717ef324cdec
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-04-16 22:34:46 +0200

    fix(modules/services): fix oData query builder
    filter empty attributes when generating oData object

--- packages/modules/services/src/context/query/generate-endpoint.ts
+++ packages/modules/services/src/context/query/generate-endpoint.ts
@@ -23,18 +23,20 @@ const buildOdataFilter = (filterObj: QueryContextOdataFilter) => {
 };
 
 const buildOdataObject = (parameters: QueryContextOdataParameters) => {
-    return Object.keys(parameters).reduce((acc, key) => {
-        switch (key) {
-            case 'filter':
-                return {
-                    ...acc,
-                    [key]: buildOdataFilter(parameters['filter'] as QueryContextOdataFilter),
-                };
-            default:
-                return { ...acc, [key]: parameters[key as keyof typeof parameters] };
-        }
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    }, {} as any);
+    return Object.entries(parameters)
+        .filter(([_, value]) => !!value)
+        .reduce((acc, [key, value]) => {
+            switch (key) {
+                case 'filter':
+                    return {
+                        ...acc,
+                        [key]: buildOdataFilter(value as QueryContextOdataFilter),
+                    };
+                default:
+                    return { ...acc, [key]: parameters[key as keyof typeof parameters] };
+            }
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        }, {} as any);
 };
 
 const createSearchParameters = (args: string | QueryContextOdataParameters) => {
