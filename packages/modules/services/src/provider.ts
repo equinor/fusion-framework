4cc31637a5d62a20b3bc887ca73faa36d5b97907
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-04-17 15:06:00 +0200

    fix(service): fix linting

--- packages/modules/services/src/provider.ts
+++ packages/modules/services/src/provider.ts
@@ -20,13 +20,12 @@ export interface IApiProvider<TClient extends IHttpClient = IHttpClient> {
     createContextClient<TMethod extends keyof ClientMethod>(
         method: TMethod
     ): Promise<ContextApiClient<TMethod, TClient>>;
-    
     /**
      * @param method - Version of the service to use
      */
     createNotificationClient<TMethod extends keyof ClientMethod>(
         method: TMethod
-    ): Promise<NotificationApiClient<TMethod, TClient>> 
+    ): Promise<NotificationApiClient<TMethod, TClient>>;
 }
 
 type ApiProviderCtorArgs<TClient extends IHttpClient = IHttpClient> = {
