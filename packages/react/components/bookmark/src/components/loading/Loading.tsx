17b179fbb25243730dd65cc116c86471074faabc
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-04-19 13:43:27 +0200

    fix(bookmark): fix linting

--- packages/react/components/bookmark/src/components/loading/Loading.tsx
+++ packages/react/components/bookmark/src/components/loading/Loading.tsx
@@ -2,15 +2,15 @@ import { CircularProgress } from '@equinor/eds-core-react';
 import React from 'react';
 
 export const Loading = () => (
-  <div
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-      width: '100%',
-    }}
-  >
-    <CircularProgress />
-  </div>
+    <div
+        style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            width: '100%',
+        }}
+    >
+        <CircularProgress />
+    </div>
 );
