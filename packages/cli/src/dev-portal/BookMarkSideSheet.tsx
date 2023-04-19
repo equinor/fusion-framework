d5da5eb2c61983c8f038956a18e7a8c16a987450
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-04-19 13:31:41 +0200

    feat(cli): added bookmark side sheet to cli and updated header

---
+++ packages/cli/src/dev-portal/BookMarkSideSheet.tsx
@@ -0,0 +1,39 @@
+import { Button, Icon } from '@equinor/eds-core-react';
+import { useFramework } from '@equinor/fusion-framework-react';
+import { Bookmark } from '@equinor/fusion-framework-react-components-bookmark';
+import { useHasBookmark } from '@equinor/fusion-framework-react-module-bookmark/portal';
+import { SideSheet } from '@equinor/fusion-react-side-sheet';
+
+type BookmarkSideSheetProps = {
+    isOpen: boolean;
+    onClose(): void;
+};
+
+export const BookmarkSideSheet = ({ isOpen, onClose }: BookmarkSideSheetProps) => {
+    const hasBookmark = useHasBookmark();
+
+    const { event } = useFramework().modules;
+
+    return (
+        <SideSheet isOpen={isOpen} onClose={onClose} isDismissable={true} enableFullscreen={true}>
+            <SideSheet.Indicator color={'#258800'} />
+            <SideSheet.Title title="Bookmarks" />
+            <SideSheet.SubTitle subTitle={'Application bookmarks'} />
+            <SideSheet.Actions>
+                <Button
+                    disabled={!hasBookmark}
+                    variant="ghost"
+                    onClick={() => {
+                        onClose();
+                        event.dispatchEvent('onBookmarkOpen', { detail: true });
+                    }}
+                >
+                    <Icon name="add" /> Add Bookmark
+                </Button>
+            </SideSheet.Actions>
+            <SideSheet.Content>
+                <Bookmark />
+            </SideSheet.Content>
+        </SideSheet>
+    );
+};
--- packages/cli/src/dev-portal/Header.tsx
+++ packages/cli/src/dev-portal/Header.tsx
@@ -1,69 +1,91 @@
 import { useState, useRef } from 'react';
-import { makeStyles, createStyles, clsx, ThemeProvider, theme } from '@equinor/fusion-react-styles';
+import { ThemeProvider, theme } from '@equinor/fusion-react-styles';
 import { ContextSelector } from './ContextSelector';
 import { FusionLogo } from './FusionLogo';
-import { Button } from '@equinor/fusion-react-button';
-import { Icon } from '@equinor/fusion-react-icon';
 
 /* typescript reference for makeStyles */
 import '@material-ui/styles';
-import { useHasBookmark } from '@equinor/fusion-framework-react-module-bookmark/portal';
+import { BookmarkSideSheet } from './BookMarkSideSheet';
+import { Button, Icon, TopBar, Typography } from '@equinor/eds-core-react';
+import { BookmarkProvider } from '@equinor/fusion-framework-react-components-bookmark';
+import { add, menu, tag } from '@equinor/eds-icons';
+import styled from '@emotion/styled';
+import { css } from '@emotion/css';
 
-const useStyles = makeStyles(
-    createStyles({
-        header: {
-            padding: '0 1em',
-        },
-        center: {
-            display: 'flex',
-            justifyContent: 'flex-start',
-            alignItems: 'center',
-        },
-        wrapper: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-        },
-        contextselector: {
-            width: '100%',
-            maxWidth: '420px',
-            marginLeft: '1em',
-        },
-    }),
-    { name: 'contextselector-styles' }
-);
+Icon.add({ menu, add, tag });
+
+const StyledHeader = styled(TopBar.Header)`
+    width: max-content;
+`;
+
+const StyledTopBar = styled(TopBar)`
+    padding: 0px;
+    height: 48px;
+`;
+
+const StyledCustomContent = styled(TopBar.CustomContent)`
+    display: flex;
+    align-items: center;
+`;
+
+const StyledActionsWrapper = styled(TopBar.Actions)`
+    padding-right: 0.5rem;
+`;
+const StyledTitle = styled(Typography)`
+    padding-left: 0.5rem;
+    white-space: nowrap;
+`;
+
+const styles = {
+    logoWrapper: css`
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    `,
+    contextSelector: css`
+        width: 100%;
+        max-width: 420px;
+        margin-left: 1em;
+    `,
+};
 
 export const Header = () => {
-    const styles = useStyles();
     const buttonRef = useRef(null);
     const [open, setOpen] = useState(false);
 
-    const hasBookmark = useHasBookmark();
+    const [isBookmarkOpen, setIsBookmarkOpen] = useState(false);
+
+    function toggleBookmark() {
+        setIsBookmarkOpen((s) => !s);
+    }
 
     return (
         <ThemeProvider theme={theme}>
-            <header className={clsx(styles.header, styles.wrapper)}>
-                <div className={clsx(styles.center)}>
-                    <Button ref={buttonRef} onClick={() => setOpen(!open)} variant="ghost">
-                        <Icon icon="menu" />
+            <StyledTopBar>
+                <StyledHeader>
+                    <Button ref={buttonRef} onClick={() => setOpen(!open)} variant="ghost_icon">
+                        <Icon name="menu" />
                     </Button>
-                    <FusionLogo scale={0.7} />
-                    <div className={clsx(styles.contextselector)}>
+                    <div className={styles.logoWrapper}>
+                        <FusionLogo scale={0.7} />
+                        <StyledTitle variant="h6">Fusion CLI</StyledTitle>
+                    </div>
+                </StyledHeader>
+                <StyledCustomContent>
+                    <div className={styles.contextSelector}>
                         <ContextSelector />
                     </div>
-                </div>
-                <div>
-                    {hasBookmark && (
-                        <Button
-                            ref={buttonRef}
-                            onClick={() => console.info('Bookmark is enabled!')}
-                            variant="ghost"
-                        >
-                            <Icon icon="tag" />
-                        </Button>
-                    )}
-                </div>
-            </header>
+                </StyledCustomContent>
+
+                <StyledActionsWrapper>
+                    <Button onClick={toggleBookmark} variant="ghost_icon">
+                        <Icon name="tag" />
+                    </Button>
+                </StyledActionsWrapper>
+            </StyledTopBar>
+            <BookmarkProvider>
+                <BookmarkSideSheet isOpen={isBookmarkOpen} onClose={toggleBookmark} />
+            </BookmarkProvider>
         </ThemeProvider>
     );
 };
