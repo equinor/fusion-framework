2722e36359c0195759490b62f4d7e193d6bddcaa
Asbjørn Håland <github@asbjornhaland.no>
2023-05-08 12:45:39 +0200

    chore: add lint for cookbooks
    Also fixes minor lint errors in the cookbooks.

--- cookbooks/app-react-router/src/Router.tsx
+++ cookbooks/app-react-router/src/Router.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 
 import { RouterProvider } from 'react-router-dom';
 import { NavigationModule } from '@equinor/fusion-framework-module-navigation';
