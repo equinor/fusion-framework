f462c77e52d1bff154caed80af56d7af6514ebf7
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-05-22 08:30:58 +0200

    fix(react-framework): expose useCurrentAppModules

--- packages/react/framework/src/app/index.ts
+++ packages/react/framework/src/app/index.ts
@@ -1,5 +1,6 @@
 export type { AppConfig, AppManifest, AppType, App } from '@equinor/fusion-framework-module-app';
 
 export { useCurrentApp } from './useCurrentApp';
+export { useCurrentAppModules } from './useCurrentAppModules';
 export { useApps } from './useApps';
 export { useAppProvider } from './useAppProvider';
