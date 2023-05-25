51049682cbc8abcb0c1326579ab3cbeadeb33595
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-05-25 12:17:28 +0200

    fix: test

--- packages/react/app/src/render-app.ts
+++ packages/react/app/src/render-app.ts
@@ -3,7 +3,7 @@ import { renderComponent, RenderTeardown } from './render-component';
 
 import type { ComponentRenderArgs } from './create-component';
 
-/** @deprecated */
+/** @deprecated  test */
 export const renderApp = (...componentArgs: Parameters<typeof createComponent>) => {
     const renderer = renderComponent(createComponent(...componentArgs));
     return (el: HTMLElement, args: ComponentRenderArgs): RenderTeardown => {
