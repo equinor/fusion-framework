3016ab0ad37fa507b12e16ad174eb33b248f02a7
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-05-27 22:51:43 +0200

    refactor(module-react-context): expose module and configurator

--- packages/react/modules/context/src/index.ts
+++ packages/react/modules/context/src/index.ts
@@ -1,8 +1,10 @@
 export {
     enableContext,
+    ContextModule,
     ContextItem,
     ContextItemType,
     IContextProvider,
+    IContextModuleConfigurator,
 } from '@equinor/fusion-framework-module-context';
 
 export { useCurrentContext, useModuleCurrentContext } from './useCurrentContext';
