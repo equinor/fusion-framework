3efbf0bb93fc11aa158872cd6ab98a22bcfb59e5
odinr <odin.rochmann@gmail.com>
2023-05-28 22:31:26 +0200

    Feat/module/add semver (#902)
    * docs(module): update tsdoc for  `Module` interface
    
    * feat(module): add semver
    
    * feat(module): add version attribute to module
    
    * feat(module): add base module provider class
    
    + create a common interface for module providers
    + create a base class for creating module provider
    
    * fix(module): add semver to legacy providers
    
    * docs(module): create change log for semver
    
    * docs(module): create changelog for base module provider

---
+++ packages/modules/module/src/lib/provider/BaseModuleProvider.ts
@@ -0,0 +1,47 @@
+import SemanticVersion from '../semantic-version';
+
+import { type IModuleProvider } from './IModuleProvider';
+
+import { Subscription, type TeardownLogic } from 'rxjs';
+
+type BaseModuleProviderCtorArgs<TConfig = unknown> = {
+    version: string;
+    config: TConfig;
+};
+
+/**
+ * Base class for creating module provider
+ *
+ * this is the interface which is returned after enabling a module
+ */
+export abstract class BaseModuleProvider<TConfig = unknown> implements IModuleProvider {
+    #version: SemanticVersion;
+    #subscriptions: Subscription;
+
+    public get version() {
+        return this.#version;
+    }
+
+    constructor(args: BaseModuleProviderCtorArgs<TConfig>) {
+        this.#version = new SemanticVersion(args.version);
+        this.#subscriptions = new Subscription();
+        this._init(args.config);
+    }
+
+    protected abstract _init(config: TConfig): void;
+
+    /**
+     * Add teardown down function, which is called on dispose.
+     *
+     * @param teardown dispose callback function
+     * @returns callback for removing the teardown
+     */
+    protected _addTeardown(teardown: Exclude<TeardownLogic, void>): VoidFunction {
+        this.#subscriptions.add(teardown);
+        return () => this.#subscriptions.remove(teardown);
+    }
+
+    public dispose() {
+        this.#subscriptions.unsubscribe();
+    }
+}
---
+++ packages/modules/module/src/lib/provider/IModuleProvider.ts
@@ -0,0 +1,6 @@
+import SemanticVersion from '../semantic-version';
+
+export interface IModuleProvider {
+    get version(): SemanticVersion;
+    dispose: VoidFunction;
+}
