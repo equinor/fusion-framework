b6ff1cd8f92d05f6a9f2e1c1b230203185c52e7b
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-07-28 14:25:15 +0200

    fix(observable): rename `useObservableEpic` to `useObservableFlow`

--- packages/utils/observable/src/react/index.ts
+++ packages/utils/observable/src/react/index.ts
@@ -1,10 +1,10 @@
 export * from './useObservable';
 export * from './useObservableEffect';
-export * from './useObservableEpic';
+export * from './useObservableInput';
+export { useObservableFlow, useObservableEpic } from './useObservableFlow';
 export * from './useObservableRef';
 export * from './useObservableState';
 export * from './useObservableSelector';
 export * from './useObservableSelectorState';
 export * from './useObservableSubscription';
-export * from './useObservableInput';
 export * from './useDebounce';
