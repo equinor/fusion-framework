f114819d3431850d2bd856bf75a9d24a055654ec
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-08-08 15:03:58 +0200

    fix(observable): use initial value from observable when using `useObservableState`

--- packages/utils/observable/src/types/index.ts
+++ packages/utils/observable/src/types/index.ts
@@ -5,7 +5,11 @@ export * from './flow';
 export * from './observable';
 export * from './reducers';
 
+export type { NestedKeys, NestedPropType } from './ts-helpers';
+
 /** @deprecated use {@link Flow}  */
 export { Flow as Epic } from './flow';
 
 export type ObservableType<T> = T extends Observable<infer U> ? U : never;
+
+export type StatefulObservable<TType> = Observable<TType> & { value: TType };
