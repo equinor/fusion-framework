6a795274524a917f316a212b67f154aa29ea3458
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-08-15 23:14:58 +0200

    chore: refactor workflows (#1136)

---
+++ .github/workflows/actions/clear-cache/clear-cache.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+while getopts r:b: flag
+do
+    case "${flag}" in
+        r) REPO=${OPTARG};;
+        b) BRANCH=${OPTARG};;
+    esac
+done
+
+set +e
+
+echo "$BRANCH"
+echo "$REPO"
+echo "Deleting caches for $REPO on barch $BRANCH"
+
+LIMIT=100
+
+function fetch_cache_records() {
+  echo "Fetching list of cache keys..."
+  CACHE_HITS=($(gh actions-cache list -R $REPO -B $BRANCH -L $LIMIT | cut -f 1))
+  CACHE_HITS_COUNT=${#CACHE_HITS[@]}
+}
+
+fetch_cache_records
+
+while [ $CACHE_HITS_COUNT -gt 0 ]
+  do
+    echo "Deleting $CACHE_HITS_COUNT items"
+    for cacheKey in "${CACHE_HITS[@]}"
+      do
+        gh actions-cache delete $cacheKey -R $REPO -B $BRANCH --confirm
+      done
+
+    if [[ "$CACHE_HITS_COUNT" == "$LIMIT" ]]; then
+      echo "Processed $LIMIT/$LIMIT, checking for more"
+      fetch_cache_records
+    else
+      CACHE_HITS_COUNT=0
+    fi
+  done
+
+echo "Done"
