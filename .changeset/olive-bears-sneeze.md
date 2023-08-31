758eaaf436ae28d180e7d91818b41abe0d9624c4
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-08-31 10:11:35 +0200

    refactor(observable): expose `castDraft` from immer

---
+++ .changeset/olive-bears-sneeze.md
@@ -0,0 +1,5 @@
+---
+'@equinor/fusion-observable': patch
+---
+
+expose `castDraft` from immer, since reused when creating reducers
--- packages/utils/observable/src/index.ts
+++ packages/utils/observable/src/index.ts
@@ -3,6 +3,8 @@
  * @module
  */
 
+export { castDraft } from 'immer';
+
 export * from './FlowSubject';
 export * from './ActionError';
 export * from './types';
