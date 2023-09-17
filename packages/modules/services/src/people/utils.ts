76eebc2a082d9b0063c600b00773209605c1804f
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-09-17 21:59:01 +0200

    fix(module-services): decrease attr check of `isApiPerson`
    api response does not match swagger, missing attr.

--- packages/modules/services/src/people/utils.ts
+++ packages/modules/services/src/people/utils.ts
@@ -6,10 +6,7 @@ import { ApiVersion } from './static';
 const requiredApiPersonAttributes = {
     [ApiVersion.v2]: [
         'azureUniqueId',
-        'accountType',
-        'invitationStatus',
-        'accountClassification',
-        'managerAzureUniqueId',
+        /** TODO - has to be more required attributes to identify V2??? */
     ] satisfies Array<keyof ApiPerson<'v2'>>,
     [ApiVersion.v4]: [
         /** TODO - has to be more required attributes to identify V4??? */
@@ -21,7 +18,7 @@ export const isApiPerson = <V extends keyof typeof ApiVersion>(version: V) => {
     /** todo add options for more strict check */
     return <T>(value: T): value is T extends ApiPerson<V> ? T : never => {
         /** early escape if value is not defined or not a object */
-        if (!!value || typeof value !== 'object') {
+        if (typeof value !== 'object') {
             return false;
         }
         const attr = Object.keys(value as object);
