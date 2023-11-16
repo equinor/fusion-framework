351b64c21506dc3ec9d448905a8899a8fadce17a
Øyvind Eikeland <oyvind@eikeland.me>
2023-11-16 12:50:33 +0100

    chore(cli): adding usericon to cli header

--- cookbooks/app-react/src/App.tsx
+++ cookbooks/app-react/src/App.tsx
@@ -5,7 +5,8 @@ export const App = () => (
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            background: '#f494b4',
+            background: '#f0f0f0',
+            color: '#343434',
         }}
     >
         <h1>🚀 Hello Fusion 😎</h1>
