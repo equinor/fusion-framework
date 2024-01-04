d3a5f2c5988deffb3d81852a69a791eeb72766fb
Øyvind Eikeland <oyvind@eikeland.me>
2024-01-04 12:14:37 +0100

    feat(featuretoggler): adds hook for getting useCurrentAppFeatureFlags

--- packages/react/framework/src/app/index.ts
+++ packages/react/framework/src/app/index.ts
@@ -2,5 +2,6 @@ export type { AppConfig, AppManifest, AppType, IApp } from '@equinor/fusion-fram
 
 export { useCurrentApp } from './useCurrentApp';
 export { useCurrentAppModules } from './useCurrentAppModules';
+export { useCurrentAppFeatureFlags } from './useCurrentAppFeatureFlags';
 export { useApps } from './useApps';
 export { useAppProvider } from './useAppProvider';
