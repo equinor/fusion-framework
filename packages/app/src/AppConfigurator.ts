34d6e897da6f19f4b6ca900e62b71206d3f3383e
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-01-29 14:02:06 +0100

    feat(fusion-framework-app): add function for using feature flags

--- packages/app/src/AppConfigurator.ts
+++ packages/app/src/AppConfigurator.ts
@@ -17,7 +17,17 @@ import http, {
 
 import auth, { configureMsal } from '@equinor/fusion-framework-module-msal';
 
-import { AppModules } from './types';
+import {
+    enableFeatureFlagging,
+    type IFeatureFlag,
+    type FeatureFlagBuilderCallback,
+} from '@equinor/fusion-framework-module-feature-flag';
+import {
+    createLocalStoragePlugin,
+    createUrlPlugin,
+} from '@equinor/fusion-framework-module-feature-flag/plugins';
+
+import { AppEnv, AppModules } from './types';
 
 /**
  * Configurator for configuring application modules
@@ -77,16 +87,38 @@ export interface IAppConfigurator<
      */
     // TODO - rename
     useFrameworkServiceClient(serviceName: string): void;
+
+    /**
+     * enable feature flagging with predefined flags
+     * @param flags array of flags
+     * @remarks
+     * this will store defined flags in local storage
+     */
+    useFeatureFlags(flags: Array<IFeatureFlag & { allowUrl?: boolean }>): void;
+
+    /**
+     * enable feature flagging with callback
+     * @param cb callback for configuring module
+     */
+    useFeatureFlags(cb: FeatureFlagBuilderCallback): void;
+
+    /**
+     * enable feature flags
+     * @remarks
+     * this function does nothing atm since api is not implemented yet
+     */
+    useFeatureFlags(): void;
 }
 
 export class AppConfigurator<
         TModules extends Array<AnyModule> | unknown = unknown,
         TRef extends FusionModulesInstance = FusionModulesInstance,
+        TEnv extends AppEnv = AppEnv,
     >
     extends ModulesConfigurator<AppModules<TModules>, TRef>
     implements IAppConfigurator<TModules, TRef>
 {
-    constructor() {
+    constructor(public readonly env: TEnv) {
         super([event, http, auth]);
         this.logger = new ModuleConsoleLogger('AppConfigurator');
     }
@@ -122,6 +154,38 @@ export class AppConfigurator<
 
     public configureMsal(...args: Parameters<typeof configureMsal>) {
         this.addConfig(configureMsal(...args));
+    }
+
+    useFeatureFlags(
+        flags_cb?:
+            | Array<IFeatureFlag<unknown> & { allowUrl?: boolean | undefined }>
+            | FeatureFlagBuilderCallback,
+    ): void {
+        switch (typeof flags_cb) {
+            case 'function': {
+                enableFeatureFlagging(this, flags_cb);
+                break;
+            }
+            case 'object': {
+                const urlFlags: IFeatureFlag[] = [];
+                const localFlags = (flags_cb ?? []).map((flag) => {
+                    const { allowUrl, ...localFlag } = flag;
+                    if (allowUrl) {
+                        urlFlags.push(flag);
+                    }
+                    return localFlag;
+                });
+                enableFeatureFlagging(this, async (builder) => {
+                    builder.addPlugin(
+                        createLocalStoragePlugin(localFlags, {
+                            name: this.env.manifest.key,
+                        }),
+                    );
+                    builder.addPlugin(createUrlPlugin(urlFlags));
+                });
+                break;
+            }
+        }
     }
 }
 
