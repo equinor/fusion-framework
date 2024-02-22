0dbe5254965bc84661a2ecced42b369ce617a758
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-02-22 09:06:42 +0100

    docs: update typing

--- packages/react/app/src/useAppModules.ts
+++ packages/react/app/src/useAppModules.ts
@@ -2,6 +2,12 @@ import type { AppModulesInstance } from '@equinor/fusion-framework-app';
 import type { AnyModule } from '@equinor/fusion-framework-module';
 import { useModules } from '@equinor/fusion-framework-react-module';
 
+/**
+ * Custom hook that returns an instance of the app modules.
+ *
+ * @template T - The type of the app modules.
+ * @returns An instance of the app modules.
+ */
 export const useAppModules = <
     T extends Array<AnyModule> | unknown = unknown,
 >(): AppModulesInstance<T> => useModules<AppModulesInstance<T>>();
