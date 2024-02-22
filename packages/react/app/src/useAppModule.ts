f3046e3e2f92b329a263e3d0476954cbf6ec6085
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-02-22 09:06:42 +0100

    docs: update typing

--- packages/react/app/src/useAppModule.ts
+++ packages/react/app/src/useAppModule.ts
@@ -1,31 +1,36 @@
 import type { AppModules, AppModulesInstance } from '@equinor/fusion-framework-app';
-import { AnyModule, ModuleKey, ModuleType, ModuleTypes } from '@equinor/fusion-framework-module';
+import type {
+    AnyModule,
+    ModuleKey,
+    ModuleType,
+    ModuleTypes,
+} from '@equinor/fusion-framework-module';
+
 import { useAppModules } from './useAppModules';
 
 /**
- * hook for getting a module from the application scope
+ * Retrieves the specified app module from the app scope.
  *
- * @template TType type of the module
- *
- * @param module name of the module, provide TType if not registered
- *
- * @returns provider of the module
+ * @template TType - The type of the app module.
+ * @template TKey - The key of the app module.
+ * @param module - The key of the app module to retrieve.
+ * @returns The app module instance if found, otherwise throws an error.
  */
-export const useAppModule = <
+export function useAppModule<
     TType extends AnyModule | unknown = unknown,
     TKey extends string = ModuleKey<ModuleTypes<AppModules<[TType]>>>,
 >(
     module: TKey,
 ): TType extends AnyModule
     ? ModuleType<TType>
-    : AppModulesInstance[Extract<keyof AppModulesInstance, TKey>] => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const appModule = useAppModules()[module];
+    : AppModulesInstance[Extract<keyof AppModulesInstance, TKey>] {
+    const appModule = useAppModules()[module as keyof AppModulesInstance];
     if (!appModule) {
         throw Error(`the requested module [${module}] is not included in the app scope`);
     }
-    return appModule;
-};
+    return appModule as TType extends AnyModule
+        ? ModuleType<TType>
+        : AppModulesInstance[Extract<keyof AppModulesInstance, TKey>];
+}
 
 export default useAppModule;
