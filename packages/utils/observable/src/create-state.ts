789619601b9a06e01de1556592a53905347cc3b6
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-03-13 09:58:54 +0100

    chore: update TypeScript version to 5.4.2

--- packages/utils/observable/src/create-state.ts
+++ packages/utils/observable/src/create-state.ts
@@ -13,7 +13,7 @@ export type FlowState<TState, TActions extends ActionDefinitions> = {
 export function createState<TState, TActions extends ActionDefinitions>(
     actions: TActions,
     reducer_or_builder:
-        | ReducerWithInitialState<TState>
+        | ReducerWithInitialState<TState, ActionTypes<TActions>>
         | {
               builder: (builder: ActionReducerMapBuilder<TState>, actions: TActions) => void;
               initial: TState | (() => TState);
@@ -23,14 +23,16 @@ export function createState<TState, TActions extends ActionDefinitions>(
     actions: TActions;
     dispatch: ReturnType<typeof actionMapper<TActions>>;
 } {
-    const reducer =
-        typeof reducer_or_builder === 'function'
-            ? reducer_or_builder
-            : createReducer(reducer_or_builder.initial as TState, (builder) =>
-                  reducer_or_builder.builder(builder, actions),
-              );
+    const reducer: ReducerWithInitialState<
+        TState,
+        ActionTypes<TActions>
+    > = typeof reducer_or_builder === 'function'
+        ? reducer_or_builder
+        : createReducer(reducer_or_builder.initial as TState, (builder) =>
+              reducer_or_builder.builder(builder, actions),
+          );
 
-    const subject = new FlowSubject<TState, ActionTypes<TActions>>(reducer);
+    const subject = new FlowSubject(reducer);
     const dispatch = actionMapper(actions, subject);
 
     return { actions, dispatch, subject };
