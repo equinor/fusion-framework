c3ae3150db9f3dd74978c6673d97d613316c0266
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-05-08 12:15:56 +0200

    chore: update context filter to throw custom error when no items are found

---
+++ .changeset/fifty-rockets-return.md
@@ -0,0 +1,5 @@
+---
+"@equinor/fusion-framework-cookbook-app-react-context-custom-error": patch
+---
+
+Updated example for custom error with context
--- cookbooks/app-react-context-custom-error/src/config.ts
+++ cookbooks/app-react-context-custom-error/src/config.ts
@@ -7,17 +7,26 @@ import {
 
 export const configure: AppModuleInitiator = (configurator) => {
     enableContext(configurator, async (builder) => {
-        builder.setContextClient({
-            get: async () => {
-                return undefined as unknown as ContextItem;
-            },
-            query: async () => {
+        builder.setContextFilter((items) => {
+            if(items.length === 0){
                 throw new FusionContextSearchError({
                     title: 'This is a custom error',
-                    description: 'This error is intentional',
+                    description: 'Could not find any items in the context. This error is intentional',
                 });
-            },
+            }
+            return items;
         });
+        // builder.setContextClient({
+        //     get: async () => {
+        //         return undefined as unknown as ContextItem;
+        //     },
+        //     query: async () => {
+        //         throw new FusionContextSearchError({
+        //             title: 'This is a custom error',
+        //             description: 'This error is intentional',
+        //         });
+        //     },
+        // });
     });
 };
 
