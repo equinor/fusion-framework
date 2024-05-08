43d409ac86687779b8b19beccfcf998bfe6c0d4e
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-05-08 14:48:58 +0200

    doc: update cookbook to check content height in dev-portal

--- cookbooks/app-react-people/src/App.tsx
+++ cookbooks/app-react-people/src/App.tsx
@@ -1,17 +1,34 @@
 import { AppRouter } from './Router';
 import { styled } from 'styled-components';
 
-const Centered = styled.div`
-    width: 860px;
-    margin: 5em auto;
-`;
+const Styled = {
+    Root: styled.div`
+        background: linear-gradient(
+            180deg,
+            rgba(0, 0, 0, 1) 0%,
+            rgba(9, 9, 121, 1) 45%,
+            rgb(135, 125, 246) 100%
+        );
+        min-height: 100%;
+        overflow: auto;
+    `,
+    Container: styled.div`
+        width: 860px;
+        /* min-height: 45vh; */
+        margin: 1em auto;
+        padding: 2em;
+        background-color: white;
+        border-radius: 7px;
+    `,
+};
 
 export const App = () => {
     return (
-        <Centered>
-            <h1>Person Components</h1>
-            <AppRouter />
-        </Centered>
+        <Styled.Root>
+            <Styled.Container>
+                <AppRouter />
+            </Styled.Container>
+        </Styled.Root>
     );
 };
 
--- cookbooks/app-react-people/src/pages/Navigation.tsx
+++ cookbooks/app-react-people/src/pages/Navigation.tsx
@@ -6,13 +6,16 @@ export const Navigation = () => {
     const navigate = useNavigate();
     return (
         <>
-            <FlexGrid>
-                <Button onClick={() => navigate('')}>Home</Button>
-                <Button onClick={() => navigate('avatar')}>PersonAvatar</Button>
-                <Button onClick={() => navigate('card')}>PersonCard</Button>
-                <Button onClick={() => navigate('list-item')}>PersonListItem</Button>
-                <Button onClick={() => navigate('selector')}>PersonSelect</Button>
-            </FlexGrid>
+            <div style={{ position: 'sticky', top: 0, background: 'white', zIndex: 1 }}>
+                <h1>People Components</h1>
+                <FlexGrid>
+                    <Button onClick={() => navigate('')}>Home</Button>
+                    <Button onClick={() => navigate('avatar')}>PersonAvatar</Button>
+                    <Button onClick={() => navigate('card')}>PersonCard</Button>
+                    <Button onClick={() => navigate('list-item')}>PersonListItem</Button>
+                    <Button onClick={() => navigate('selector')}>PersonSelect</Button>
+                </FlexGrid>
+            </div>
             <Outlet></Outlet>
         </>
     );
