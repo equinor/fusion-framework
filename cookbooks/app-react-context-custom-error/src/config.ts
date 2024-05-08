c1617d3fdbe3d7560dd30bf0785b3730e2d7527a
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-05-08 12:58:31 +0200

    chore: update cookbook (#2133)
    * chore: update context filter to throw custom error when no items are found
    
    * style: cleanup
    
    * chore: update pr workflow

--- cookbooks/app-react-context-custom-error/src/config.ts
+++ cookbooks/app-react-context-custom-error/src/config.ts
@@ -1,23 +1,32 @@
 import type { AppModuleInitiator } from '@equinor/fusion-framework-react-app';
 import {
-    ContextItem,
     FusionContextSearchError,
     enableContext,
 } from '@equinor/fusion-framework-react-module-context';
 
 export const configure: AppModuleInitiator = (configurator) => {
     enableContext(configurator, async (builder) => {
-        builder.setContextClient({
-            get: async () => {
-                return undefined as unknown as ContextItem;
-            },
-            query: async () => {
+        builder.setContextFilter((items) => {
+            if (items.length === 0) {
                 throw new FusionContextSearchError({
                     title: 'This is a custom error',
-                    description: 'This error is intentional',
+                    description:
+                        'Could not find any items in the context. This error is intentional',
                 });
-            },
+            }
+            return items;
         });
+        // builder.setContextClient({
+        //     get: async () => {
+        //         return undefined as unknown as ContextItem;
+        //     },
+        //     query: async () => {
+        //         throw new FusionContextSearchError({
+        //             title: 'This is a custom error',
+        //             description: 'This error is intentional',
+        //         });
+        //     },
+        // });
     });
 };
 
