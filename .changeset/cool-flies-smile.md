3cd63d58e4e3ffd15bccdebaa94b391e3e3d12f0
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-05-13 19:45:21 +0200

    Fix(cli): portal height (#2135)
    * fix: style rendering issue for application root element
    
    * doc: update cookbook to check content height in dev-portal

---
+++ .changeset/cool-flies-smile.md
@@ -0,0 +1,7 @@
+---
+"@equinor/fusion-framework-cookbook-app-react-people": patch
+---
+
+Updated the React people cookbook to handle content height correctly in the dev portal
+
+The React people cookbook previously had an issue where the content height was not calculated correctly when running in the dev portal environment. This changeset fixes that issue by adjusting the height calculation logic to work properly in the dev portal.
--- cookbooks/app-react-people/src/pages/ListItemPage.tsx
+++ cookbooks/app-react-people/src/pages/ListItemPage.tsx
@@ -1,13 +1,46 @@
-import { PersonListItem } from '@equinor/fusion-react-person';
+import { useEffect, useState } from 'react';
+import { PersonListItem, type PersonInfo } from '@equinor/fusion-react-person';
 import { FlexGridColumn } from '../Styled';
 
+/** DEMO HACK */
+const useDemoData = (search: string) => {
+    const [persons, setPersons] = useState<PersonInfo[]>([]);
+
+    useEffect(() => {
+        const providerEl = document.querySelector('fwc-person-provider');
+        if (providerEl) {
+            const controller = new AbortController();
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            providerEl.resolver
+                .search({ search, signal: controller.signal })
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                .then(setPersons);
+            return () => controller.abort();
+        }
+    }, [search]);
+
+    return persons;
+};
+
 export const ListItemPage = () => {
+    const persons = useDemoData('FOIT CON PDL');
+
     return (
         <>
             <h2>PersonListItem</h2>
             <FlexGridColumn>
-                <PersonListItem azureId="cbc6480d-12c1-467e-b0b8-cfbb22612daa" />
-                <PersonListItem upn="handah@equinor.com" />
+                {persons.length === 0 ? (
+                    <>
+                        <PersonListItem azureId="cbc6480d-12c1-467e-b0b8-cfbb22612daa" />
+                        <PersonListItem upn="handah@equinor.com" />
+                    </>
+                ) : (
+                    persons.map((person) => (
+                        <PersonListItem key={person.azureId} dataSource={person} />
+                    ))
+                )}
             </FlexGridColumn>
         </>
     );
