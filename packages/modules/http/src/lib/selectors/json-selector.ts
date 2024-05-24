1e60919e83fb65528c88f604d7bd43299ec412e1
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-05-24 11:13:09 +0200

    fix(http)!: throw correct error in jsonSelector (#2196)
    The `jsonSelector` function is not checking the error type in the `catch`
    block. This led to always throwing a parser error, even when the actual
    error was due to an unsuccessful response.
    
    BREAKING CHANGE:
    If for some reason developers has catched the error and assumed the cause property would give the proper error data, this will no longer be the case.

--- packages/modules/http/src/lib/selectors/json-selector.ts
+++ packages/modules/http/src/lib/selectors/json-selector.ts
@@ -1,30 +1,46 @@
 import { HttpJsonResponseError } from '../../errors';
 
 /**
- * Converts the JSON response from an HTTP request into the specified type.
- * If the response is not OK or if there is an error parsing the response, an error is thrown.
- * If the response has a status code of 204 (No Content), a resolved Promise is returned.
+ * Asynchronously parses the JSON data from a given HTTP response.
  *
- * @param response The HTTP response object.
- * @returns A Promise that resolves to the parsed JSON data.
- * @throws {Error} If the network response is not OK.
- * @throws {HttpJsonResponseError} If there is an error parsing the response.
+ * If the response has a status code of 204 (No Content), this function will resolve with `undefined`.
+ *
+ * If the response is not successful (i.e. `response.ok` is `false`), this function will throw an `HttpJsonResponseError` with the response details and the parsed data (if any).
+ *
+ * If there is an error parsing the JSON data, this function will throw an `HttpJsonResponseError` with the parsing error and the original response.
+ *
+ * @template TType - The expected type of the parsed JSON data.
+ * @template TResponse - The type of the HTTP response object.
+ * @param response - The HTTP response to parse.
+ * @returns A promise that resolves with the parsed JSON data, or rejects with an `HttpJsonResponseError`.
  */
 export const jsonSelector = async <TType = unknown, TResponse extends Response = Response>(
     response: TResponse,
 ): Promise<TType> => {
-    /** Status code 204 is no content */
+    /** Status code 204 indicates no content in the response */
     if (response.status === 204) {
         return Promise.resolve() as Promise<TType>;
     }
 
     try {
+        // Parse the response JSON data
         const data = await response.json();
+
+        // Check if the response was successful
         if (!response.ok) {
+            // Throw an error with the response details
             throw new HttpJsonResponseError('network response was not OK', response, { data });
         }
+
+        // Return the parsed data
         return data;
     } catch (cause) {
+        // If the cause is an HttpJsonResponseError, rethrow it
+        if (cause instanceof HttpJsonResponseError) {
+            throw cause;
+        }
+
+        // Otherwise, throw a new HttpJsonResponseError with the parsing error
         throw new HttpJsonResponseError('failed to parse response', response, { cause });
     }
 };
