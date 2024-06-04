d2e3ecaec7525f027f9d73e97066b9af2e01ff46
Andrej Nikolic <andrej.nikolic@bouvet.no>
2024-06-04 12:38:44 +0200

    fix(cookbook): ag grid cookbook updated example

--- cookbooks/app-react-ag-grid/src/App.tsx
+++ cookbooks/app-react-ag-grid/src/App.tsx
@@ -1,25 +1,50 @@
+import { useCallback, useMemo, useState } from 'react';
 import { AgGridReact } from '@ag-grid-community/react';
 import useStyles from '@equinor/fusion-react-ag-grid-styles';
-import { useCallback, useMemo, useState } from 'react';
-
-import { ModuleRegistry } from '@ag-grid-community/core';
+import { ColDef, ModuleRegistry } from '@ag-grid-community/core';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+import { ClipboardModule } from '@ag-grid-enterprise/clipboard';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { defaultColDef, sideBar } from './table';
 
-ModuleRegistry.registerModules([ClientSideRowModelModule]);
+ModuleRegistry.registerModules([
+    ClientSideRowModelModule,
+    ColumnsToolPanelModule,
+    FiltersToolPanelModule,
+    MenuModule,
+    ExcelExportModule,
+    RangeSelectionModule,
+    ClipboardModule,
+]);
+
+export type RowDataType = {
+    make: string;
+    model: string;
+    price: number;
+};
 
 export const App = (): JSX.Element => {
     const styles = useStyles();
+    const gridStyle = useMemo(() => ({ height: '500px', width: '100%' }), []);
 
-    const [rowData, setRowData] = useState([
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxster', price: 72000 },
-    ]);
+    const [rowData, setRowData] = useState<RowDataType[]>([]);
 
-    const columnDefs = useMemo(
-        () => [{ field: 'make' }, { field: 'model' }, { field: 'price' }],
-        [],
-    );
+    const columnDefs = useMemo<ColDef[]>(() => {
+        return [
+            {
+                field: 'make',
+                headerCheckboxSelection: true,
+                checkboxSelection: true,
+                showDisabledCheckboxes: true,
+            },
+            { field: 'model' },
+            { field: 'price' },
+        ];
+    }, []);
 
     const addRow = useCallback(() => {
         setRowData([
@@ -32,17 +57,33 @@ export const App = (): JSX.Element => {
         ]);
     }, [rowData, setRowData]);
 
+    const onGridReady = useCallback(() => {
+        setRowData([
+            { make: 'Toyota', model: 'Celica', price: 35000 },
+            { make: 'Ford', model: 'Mondeo', price: 32000 },
+            { make: 'Porsche', model: 'Boxster', price: 72000 },
+        ]);
+    }, []);
+
     return (
         <>
             <div>
                 <h4>Hello Fusion-framework Ag-Grid</h4>
                 <button onClick={addRow}>Add Row</button>
             </div>
-            <div
-                className={[styles.root, 'ag-theme-alpine-fusion'].join(' ')}
-                style={{ height: 500 }}
-            >
-                <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+            <div style={gridStyle} className={`${styles.root} ag-theme-alpine-fusion`}>
+                <AgGridReact
+                    rowData={rowData}
+                    columnDefs={columnDefs}
+                    defaultColDef={defaultColDef}
+                    sideBar={sideBar}
+                    rowSelection={'multiple'}
+                    suppressRowClickSelection={true}
+                    enableRangeSelection={true}
+                    copyHeadersToClipboard={true}
+                    allowContextMenuWithControlKey={true}
+                    onGridReady={onGridReady}
+                />
             </div>
         </>
     );
