f0b997d9df92cc82ea0255012fa25ba24b830e6c
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-07-01 14:44:03 +0200

    wip

--- packages/react/modules/bookmark/src/index.ts
+++ packages/react/modules/bookmark/src/index.ts
@@ -1,4 +1,3 @@
-export * from './types';
 export { default, useCurrentBookmark } from './useCurrentBookmark';
 export { useBookmark } from './useBookmark';
-export type { PatchBookmark } from '@equinor/fusion-framework-module-bookmark';
+export type { BookmarkUpdate } from '@equinor/fusion-framework-module-bookmark';
