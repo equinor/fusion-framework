a1524e9c4d83778da3db42dbcf99908b776a0592
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-07-11 14:23:53 +0200

    feat: Fix type inference for dot path in utils/dot-path.ts (#2356)

--- packages/modules/module/src/utils/dot-path.ts
+++ packages/modules/module/src/utils/dot-path.ts
@@ -1,9 +1,7 @@
 export type DotPath<TObject extends object> = {
-    [Key in keyof TObject & string]: TObject[Key] extends CallableFunction
-        ? never
-        : TObject[Key] extends object
-          ? `${Key}` | `${Key}.${DotPath<TObject[Key]>}`
-          : `${Key}`;
+    [Key in keyof TObject & string]: TObject[Key] extends object
+        ? `${Key}` | `${Key}.${DotPath<TObject[Key]>}`
+        : `${Key}`;
 }[keyof TObject & string];
 
 export type DotPathType<TType, TPath extends string> = TPath extends keyof TType
