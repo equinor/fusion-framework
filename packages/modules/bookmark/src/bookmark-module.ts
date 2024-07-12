def825ab562ac37b73747e32f5b265204ad3df00
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-07-12 10:27:39 +0200

    Feat(module bookmark): rewrite (#2355)
    * feat(module-bookmark): rework bookmark module
    
    Needed rewrite of the bookmark module to better represent the api, and provide a more robust interface for working with bookmarks. Instead of fixing the current implementation, it was decided to rework the entire module to save time and confusion in the future.
    
    BREAKING CHANGES:
    - new configurator
    - new provider interface

---
+++ packages/modules/bookmark/src/bookmark-module.ts
@@ -0,0 +1,60 @@
+import { Module, ModulesInstance, SemanticVersion } from '@equinor/fusion-framework-module';
+import { EventModule } from '@equinor/fusion-framework-module-event';
+import { ServicesModule } from '@equinor/fusion-framework-module-services';
+import { AppModule } from '@equinor/fusion-framework-module-app';
+import { ContextModule } from '@equinor/fusion-framework-module-context';
+import { BookmarkProvider } from './BookmarkProvider';
+import { BookmarkModuleConfigurator } from './BookmarkConfigurator';
+import { ConsoleLogger, ILogger } from '@equinor/fusion-log';
+import { lastValueFrom } from 'rxjs';
+import { version } from './version';
+
+export type BookmarkModuleKey = 'bookmark';
+
+export const moduleKey: BookmarkModuleKey = 'bookmark';
+
+export type BookmarkModule = Module<
+    BookmarkModuleKey,
+    BookmarkProvider,
+    BookmarkModuleConfigurator,
+    [EventModule, ServicesModule, AppModule, ContextModule]
+>;
+
+// TODO - remove when all framework uses log
+const fallbackLogger: ILogger = new ConsoleLogger('BookmarkModule');
+
+export const module: BookmarkModule = {
+    name: moduleKey,
+    version: new SemanticVersion(version),
+    configure: (args) => {
+        // use parent logger if available, else fallback to console logger
+        const log: ILogger =
+            (args?.log as ILogger)?.createSubLogger('BookmarkModule') || fallbackLogger;
+
+        // Set client from parent module if available
+        const ref = args.ref as ModulesInstance<[BookmarkModule]>;
+
+        // create a configurator instance
+        const configurator = new BookmarkModuleConfigurator({ log, ref });
+
+        return configurator;
+    },
+    initialize: async (args) => {
+        const config = await lastValueFrom(
+            (args.config as BookmarkModuleConfigurator).createConfig(args),
+        );
+        const provider = new BookmarkProvider(config);
+        return provider;
+    },
+    dispose: (args) => {
+        (args.instance as BookmarkProvider).dispose();
+    },
+};
+
+declare module '@equinor/fusion-framework-module' {
+    interface Modules {
+        bookmark: BookmarkModule;
+    }
+}
+
+export default module;
---
+++ packages/modules/bookmark/src/bookmark.schemas.ts
@@ -0,0 +1,49 @@
+import * as z from 'zod';
+
+const bookmarkUserSchema = z.object({
+    azureUniqueId: z.string(),
+    name: z.string(),
+    mail: z.string().optional(),
+    phoneNumber: z.string().optional(),
+    jobTitle: z.string().optional(),
+    accountType: z.enum(['Employee', 'Consultant', 'External', 'Application', 'Local']).optional(),
+    accountClassification: z.enum(['Unclassified', 'Internal', 'External']).optional(),
+});
+
+export const bookmarkSourceSystemSchema = z.object({
+    identifier: z.string(),
+    name: z.string().optional(),
+    subSystem: z.string().optional(),
+});
+
+const bookmarkContextSchema = z.object({
+    id: z.string(),
+    name: z.string().optional(),
+    type: z.string().optional(),
+});
+
+export const bookmarkSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    appKey: z.string(),
+    description: z.string().optional(),
+    isShared: z.boolean().optional(),
+    created: z.date(),
+    createdBy: bookmarkUserSchema,
+    updated: z.date().optional(),
+    updatedBy: bookmarkUserSchema.optional(),
+    context: bookmarkContextSchema.optional(),
+    sourceSystem: bookmarkSourceSystemSchema.optional(),
+});
+
+export const bookmarksSchema = z.array(bookmarkSchema);
+
+export const bookmarkWithDataSchema = <
+    T extends Record<string, unknown>,
+    S extends z.ZodSchema<T> = z.ZodSchema<T>,
+>(
+    schema: S = z.record(z.unknown()).default({}) as unknown as S,
+) =>
+    bookmarkSchema.extend({
+        payload: schema,
+    });
