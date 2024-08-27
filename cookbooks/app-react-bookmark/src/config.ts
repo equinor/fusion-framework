d8c4a6a06cd107f2e6e24b9de39997e43606f52e
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-08-27 10:22:59 +0200

    feat(cookbook-react-bookmark): Update cookbook to use new bookmark module

--- cookbooks/app-react-bookmark/src/config.ts
+++ cookbooks/app-react-bookmark/src/config.ts
@@ -1,22 +1,13 @@
 import type { AppModuleInitiator } from '@equinor/fusion-framework-react-app';
 
 import { enableContext } from '@equinor/fusion-framework-module-context';
+import { enableBookmark } from '@equinor/fusion-framework-react-app/bookmark';
 
 export const configure: AppModuleInitiator = (configurator) => {
     enableContext(configurator, async (builder) => {
         builder.setContextType(['projectMaster']); // set contextType to match against
     });
-    configurator.logger.level = 0;
-
-    /** callback when configurations is created */
-    configurator.onConfigured((config) => {
-        console.log('application config created', config);
-    });
-
-    /** callback when the application modules has initialized */
-    configurator.onInitialized((instance) => {
-        console.log('application config initialized', instance);
-    });
+    enableBookmark(configurator);
 };
 
 export default configure;
