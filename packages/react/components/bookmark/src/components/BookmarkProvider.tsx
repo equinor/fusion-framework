7a5a55034df6c5c84cfe8bc77c48ff5cf861b347
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-08-27 10:22:59 +0200

    feat(rect-components-bookmark): Make component to use provided bookmark provider
    - BookmarkProvider should have provider as a parameter
    - BookmarkProvider should share context of provided provider to child components
    - Remove deprecated hook
    - Update component to align with changes in modules
    
    BREAKING CHANGES: BookmarkProvider now requires provider as parameter

--- packages/react/components/bookmark/src/components/BookmarkProvider.tsx
+++ packages/react/components/bookmark/src/components/BookmarkProvider.tsx
@@ -1,30 +1,84 @@
 import { Snackbar } from '@equinor/eds-core-react';
 import type { FrameworkEvent, FrameworkEventInit } from '@equinor/fusion-framework-module-event';
 import { useFramework } from '@equinor/fusion-framework-react';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { CreateBookmarkModal } from './create-bookmark';
 import { EditBookmarkModal } from './edit-bookmark';
 import { ImportBookmarkModal } from './import-bookmark';
+import {
+    BookmarkModule,
+    type BookmarkProvider as IBookmarkProvider,
+} from '@equinor/fusion-framework-module-bookmark';
+import { Observable, of } from 'rxjs';
+import { useObservableState } from '@equinor/fusion-observable/react';
 
-export const BookmarkProvider = ({ children }: PropsWithChildren<unknown>) => {
+type BookmarkApp = {
+    appKey: string;
+    name?: string;
+};
+
+type ProviderState = {
+    provider: IBookmarkProvider;
+    currentApp?: BookmarkApp | null;
+};
+
+const bookmarkProviderContext = createContext<ProviderState | null>(null);
+
+type BookmarkProviderProps = {
+    readonly provider?: IBookmarkProvider | null;
+};
+
+export const useBookmarkComponentContext = () =>
+    useContext(bookmarkProviderContext) as ProviderState;
+
+export const useBookmarkProvider = () => useBookmarkComponentContext().provider;
+
+export const BookmarkProvider = (props: PropsWithChildren<BookmarkProviderProps>) => {
+    const { event: eventProvider } = useFramework<[BookmarkModule]>().modules;
+
+    const { provider, children } = props;
+
     const [isCreateBookmarkOpen, setIsCreateBookmarkOpen] = useState(false);
     const [isEditBookmarkOpen, setIsEditBookmarkOpen] = useState(false);
     const [editBookmarkId, setEditBookmarkId] = useState<string | undefined>();
     const [isBookmarkCopy, setIsBookmarkCopy] = useState(false);
 
-    const { event } = useFramework().modules;
+    // get the current app
+    const { value: currentApp } = useObservableState(
+        useMemo(() => {
+            // if no provider, return null, this should not happen
+            if (!provider) {
+                return of(null);
+            }
+            return new Observable<BookmarkApp | null>((observer) => {
+                // resolve the application
+                const resolveApp = async () => {
+                    // get the resolved application
+                    const app = await provider.resolvedApplication();
+                    // emit the resolved application
+                    observer.next(app);
+                };
+                // re-resolve the application when the current app changes
+                eventProvider.addEventListener('onCurrentAppChanged', resolveApp);
+
+                // initial resolve
+                resolveApp();
+            });
+        }, [eventProvider, provider]),
+        { initial: undefined },
+    );
 
     useEffect(() => {
-        const subOnBookmarkOpen = event.addEventListener('onBookmarkOpen', (e) => {
+        const subOnBookmarkOpen = eventProvider.addEventListener('onBookmarkOpen', (e) => {
             setIsCreateBookmarkOpen(e.detail);
         });
 
-        const subOnBookmarkEdit = event.addEventListener('onBookmarkEdit', (e) => {
+        const subOnBookmarkEdit = eventProvider.addEventListener('onBookmarkEdit', (e) => {
             setEditBookmarkId(e.detail.bookmarkId);
             setIsEditBookmarkOpen(true);
         });
 
-        const subOnBookmarkUrlCopy = event.addEventListener('onBookmarkUrlCopy', () => {
+        const subOnBookmarkUrlCopy = eventProvider.addEventListener('onBookmarkUrlCopy', () => {
             setIsBookmarkCopy(true);
         });
 
@@ -33,16 +87,22 @@ export const BookmarkProvider = ({ children }: PropsWithChildren<unknown>) => {
             subOnBookmarkOpen();
             subOnBookmarkUrlCopy();
         };
-    }, [event]);
+    }, [eventProvider]);
+
+    if (!provider) {
+        return null;
+    }
 
     return (
-        <>
+        <bookmarkProviderContext.Provider value={{ provider, currentApp }}>
             <CreateBookmarkModal isOpen={isCreateBookmarkOpen} onClose={setIsCreateBookmarkOpen} />
-            <EditBookmarkModal
-                isOpen={isEditBookmarkOpen}
-                onClose={setIsEditBookmarkOpen}
-                bookmarkId={editBookmarkId}
-            />
+            {editBookmarkId && (
+                <EditBookmarkModal
+                    isOpen={isEditBookmarkOpen}
+                    onClose={setIsEditBookmarkOpen}
+                    bookmarkId={editBookmarkId}
+                />
+            )}
             <ImportBookmarkModal />
             <Snackbar
                 autoHideDuration={2000}
@@ -52,7 +112,7 @@ export const BookmarkProvider = ({ children }: PropsWithChildren<unknown>) => {
                 Bookmark url copied to clipboard
             </Snackbar>
             {children}
-        </>
+        </bookmarkProviderContext.Provider>
     );
 };
 
