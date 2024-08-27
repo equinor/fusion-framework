cda064916a8e9880e145c32d2cdc66ca7f1ca6a6
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-08-27 10:22:59 +0200

    feat(rect-components-bookmark): Make component to use provided bookmark provider
    - BookmarkProvider should have provider as a parameter
    - BookmarkProvider should share context of provided provider to child components
    - Remove deprecated hook
    - Update component to align with changes in modules
    
    BREAKING CHANGES: BookmarkProvider now requires provider as parameter

--- packages/react/components/bookmark/src/components/Bookmark.tsx
+++ packages/react/components/bookmark/src/components/Bookmark.tsx
@@ -1,8 +1,8 @@
-import { useBookmark } from '@equinor/fusion-framework-react-module-bookmark';
+import { EMPTY } from 'rxjs';
 import { useBookmarkGrouping } from '../hooks';
 import { BookmarkFilter } from './filter/Filter';
 import { SectionList } from './sectionList/SectionList';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { Icon } from '@equinor/eds-core-react';
 import { chevron_down, chevron_right, share, more_vertical, add } from '@equinor/eds-icons';
@@ -10,7 +10,8 @@ import { chevron_down, chevron_right, share, more_vertical, add } from '@equinor
 import styled from 'styled-components';
 import { Message } from './messages/Message';
 import { Loading } from './loading/Loading';
-import { useFramework } from '@equinor/fusion-framework-react';
+import { useBookmarkComponentContext } from './BookmarkProvider';
+import { useObservableState } from '@equinor/fusion-observable/react';
 
 Icon.add({
     chevron_down,
@@ -42,29 +43,51 @@ const Styled = {
 };
 
 export const Bookmark = () => {
-    const { bookmarks, getAllBookmarks } = useBookmark();
+    const { provider } = useBookmarkComponentContext();
+
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
 
-    const { event } = useFramework().modules;
+    const { value: bookmarks } = useObservableState(
+        useMemo(() => provider?.bookmarks$ || EMPTY, [provider]),
+        { initial: provider?.bookmarks },
+    );
 
     useEffect(() => {
-        return event.addEventListener('onBookmarksChanged', () => {
-            setLoading(false);
+        const sub = provider?.getAllBookmarks().subscribe({
+            complete: () => {
+                setLoading(false);
+            },
+            error: (err) => {
+                setError(err);
+            },
         });
-    }, [event]);
-
-    useEffect(() => {
-        if (bookmarks.length > 0) {
-            setLoading(false);
-        }
-    }, [bookmarks]);
-
-    useEffect(() => {
-        getAllBookmarks();
-    }, [getAllBookmarks]);
+        return () => sub?.unsubscribe();
+    }, [provider]);
 
     const { bookmarkGroups, groupingModes, searchText, setGroupBy, setSearchText, groupByKey } =
         useBookmarkGrouping(bookmarks);
+
+    const content = useMemo(() => {
+        if (error) {
+            return (
+                <Message title="Error" type="Error">
+                    {error.message}
+                </Message>
+            );
+        }
+        if (loading) {
+            return <Loading />;
+        }
+        if (bookmarkGroups.length === 0) {
+            return (
+                <Message title="No Bookmarks" type="NoContent">
+                    You have not created any bookmarks yet.
+                </Message>
+            );
+        }
+        return <SectionList bookmarkGroups={bookmarkGroups} />;
+    }, [error, loading, bookmarkGroups]);
 
     return (
         <Styled.Wrapper>
@@ -75,19 +98,7 @@ export const Bookmark = () => {
                 setGroupBy={setGroupBy}
                 setSearchText={setSearchText}
             />
-            <Styled.List>
-                {bookmarkGroups.length > 0 ? (
-                    <SectionList bookmarkGroups={bookmarkGroups} />
-                ) : loading ? (
-                    <Loading />
-                ) : (
-                    <Styled.NoContentWrapper>
-                        <Message title="No Bookmarks" type="NoContent">
-                            You have not created any bookmarks yet.
-                        </Message>
-                    </Styled.NoContentWrapper>
-                )}
-            </Styled.List>
+            <Styled.List>{content}</Styled.List>
         </Styled.Wrapper>
     );
 };
