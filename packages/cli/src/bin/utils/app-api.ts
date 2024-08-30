a3305d505a7b1b74a66dc4796432534047256c83
Øyvind Eikeland <oyvind@eikeland.me>
2024-08-30 13:27:25 +0200

    fix(app): setting app api version and exposing baseUri in app configurator

--- packages/cli/src/bin/utils/app-api.ts
+++ packages/cli/src/bin/utils/app-api.ts
@@ -10,7 +10,7 @@ export const getEndpointUrl = async (
     endpoint: string,
     fusionEnv: FusionEnv,
     service: string,
-    version: string = '1.0-preview',
+    version: string = '1.0',
 ) => {
     const { CUSTOM_APPAPI, FUSION_CLI_ENV, FUSION_TOKEN } = process.env;
 
