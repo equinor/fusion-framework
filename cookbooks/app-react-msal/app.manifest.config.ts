58890621a6745e9fd72740794d8df9868206542c
Øyvind Eikeland <oyvind@eikeland.me>
2024-09-05 09:22:06 +0200

    fix(cookbooks): adjusting app config and manifest to breaking changes in cookbooks

--- cookbooks/app-react-msal/app.manifest.config.ts
+++ cookbooks/app-react-msal/app.manifest.config.ts
@@ -2,9 +2,7 @@ import { defineAppManifest, mergeManifests } from '@equinor/fusion-framework-cli
 
 export default defineAppManifest((env, { base }) => {
     if (env.command === 'serve') {
-        return mergeManifests(base, {
-            key: 'msal',
-        });
+        return mergeManifests(base, {});
     }
     return base;
 });
