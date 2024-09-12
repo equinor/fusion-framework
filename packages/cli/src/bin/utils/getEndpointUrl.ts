2529e812a54849f2102db8e9754de2b3dc655325
Øyvind Eikeland <oyvind@eikeland.me>
2024-09-12 11:24:12 +0200

    feat(cli): refactoring and error handling in commands using apps-service

---
+++ packages/cli/src/bin/utils/getEndpointUrl.ts
@@ -0,0 +1,56 @@
+export type FusionEnv = 'ci' | 'fqa' | 'tr' | 'fprd';
+
+/**
+ * Retreive full endpoint URI to env in service-discovery
+ * @param endpoint <string> The endpoint to call in+ uri
+ * @param fusionEnv <FusionEnv> The Fusion env to get uri for
+ * @param service <string> Custom service uri to use insted of Fusion
+ * @param version <string> The version of the api to use
+ * @returns <string> The uri with endpoint
+ */
+export const getEndpointUrl = async (
+    endpoint: string,
+    fusionEnv: FusionEnv,
+    service: string,
+    version: string = '1.0',
+): Promise<string> => {
+    const { CUSTOM_APPAPI, FUSION_CLI_ENV, FUSION_TOKEN } = process.env;
+
+    /* use consumer provided api url */
+    if (service || CUSTOM_APPAPI) {
+        return service ?? CUSTOM_APPAPI;
+    }
+
+    /* Env has changed get new api url */
+    if (FUSION_CLI_ENV !== fusionEnv || !process.env.FUSION_CLI_APPAPI) {
+        process.env.FUSION_CLI_ENV = fusionEnv;
+
+        const requestService = await fetch(
+            `https://discovery.ci.fusion-dev.net/service-registry/environments/${fusionEnv}/services/apps`,
+            {
+                headers: {
+                    Authorization: `Bearer ${FUSION_TOKEN}`,
+                },
+            },
+        );
+
+        if (requestService.status === 401) {
+            throw new Error(`The provided FUSION_TOKEN is not valid in environment: ${fusionEnv}`);
+        }
+
+        if (!requestService.ok) {
+            throw new Error(
+                `Failed getEndpointUrl from service-discovery. HTTP status: ${requestService.status} - ${requestService.statusText}`,
+            );
+        }
+
+        const responseService = await requestService.json();
+        process.env.FUSION_CLI_APPAPI = responseService.uri;
+    }
+
+    const uri = new URL(`${process.env.FUSION_CLI_APPAPI}/${endpoint}`);
+    uri.searchParams.set('api-version', version);
+
+    /* return fresh/cached endpoint url */
+    return uri.href;
+};
---
+++ packages/cli/src/bin/utils/index.ts
@@ -0,0 +1,18 @@
+export { getEndpointUrl } from './getEndpointUrl.js';
+export type { FusionEnv } from './getEndpointUrl.js';
+export { requireToken } from './requireToken.js';
+export { isAppRegistered } from './isAppRegistered.js';
+
+export { loadAppConfig } from './load-app-config.js';
+export { loadAppManifest } from './load-manifest.js';
+export { loadPackage } from './load-package.js';
+export { loadViteConfig } from './load-vite-config.js';
+
+export { formatPath, formatByteSize } from './format.js';
+export { executeCommand } from './execute-command.js';
+
+export { publishAppConfig } from './publishAppConfig.js';
+export { tagAppBundle } from './tagAppBundle.js';
+export { uploadAppBundle } from './uploadAppBundle.js';
+
+export { Spinner } from './spinner.js';
