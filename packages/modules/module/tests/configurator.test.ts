4e61b479ecfa0f04a799540d46a8cd6d02b826c6
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-09-26 13:53:35 +0200

    refactor(module): optimize configurator tests

--- packages/modules/module/tests/configurator.test.ts
+++ packages/modules/module/tests/configurator.test.ts
@@ -1,36 +1,48 @@
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, Mock, vi } from 'vitest';
 import { ModulesConfigurator } from '../src/configurator';
 import { type Module } from '../src/types';
 
-const testModule: Module<'tester', object, void> = {
+type MockModule<TName extends string, TType extends object, TConfig = unknown> = {
+    name: string;
+    initialize: Mock<Module<TName, TType, TConfig>['initialize']>;
+    configure?: Mock<Required<Module<TName, TType, TConfig>>['configure']>;
+};
+
+type TestModule = MockModule<'tester', object>;
+
+const testModule: TestModule = {
     name: 'tester',
     initialize: vi.fn(() => ({})),
 };
 
+let testConfigurator: ModulesConfigurator<[TestModule]>;
+
 describe('ModulesConfigurator', () => {
+    beforeEach(() => {
+        testConfigurator = new ModulesConfigurator([testModule]);
+    });
     afterEach(() => {
         vi.clearAllMocks();
     });
 
-    it('should create a new instance', () => {
-        const configurator = new ModulesConfigurator();
-        expect(configurator).toBeInstanceOf(ModulesConfigurator);
-    });
-
-    it('should create instance with initial configuration', () => {
-        const configurator = new ModulesConfigurator([testModule]);
-        expect(configurator.modules).toHaveLength(1);
+    it('should have a configurator with one module registered', () => {
+        expect(testConfigurator).toBeInstanceOf(ModulesConfigurator);
+        expect(testConfigurator.modules).toHaveLength(1);
     });
 
     it('should allow adding configuration', () => {
-        const configurator = new ModulesConfigurator();
-        configurator.addConfig({ module: testModule });
-        expect(configurator.modules).toHaveLength(1);
+        testConfigurator.addConfig({
+            module: {
+                name: 'tester2',
+                initialize: () => ({}),
+            },
+        });
+        expect(testConfigurator.modules).toHaveLength(2);
     });
 
     it('should create an instance', async () => {
-        const configurator = new ModulesConfigurator([testModule]);
-        const instance = await configurator.initialize();
+        const instance = await testConfigurator.initialize();
         expect(instance).toHaveProperty('tester');
+        expect(testModule.initialize).toHaveBeenCalledOnce();
     });
 });
