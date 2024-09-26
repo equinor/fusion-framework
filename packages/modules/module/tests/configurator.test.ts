50ebe0045b60d5251bed642342d2794b5c437cdc
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-09-26 12:33:22 +0200

    test(module): should create instance with initial configuration

--- packages/modules/module/tests/configurator.test.ts
+++ packages/modules/module/tests/configurator.test.ts
@@ -1,9 +1,18 @@
 import { describe, expect, it } from 'vitest';
 import { ModulesConfigurator } from '../src/configurator';
+import { AnyModule } from '../src/types';
 
 describe('ModulesConfigurator', () => {
     it('should create a new instance', () => {
         const configurator = new ModulesConfigurator();
         expect(configurator).toBeInstanceOf(ModulesConfigurator);
+    });
+    it('should create instance with initial configuration', () => {
+        const testModule: AnyModule = {
+            name: 'tester',
+            initialize: () => {},
+        };
+        const configurator = new ModulesConfigurator([testModule]);
+        expect(configurator.modules).toHaveLength(1);
     });
 });
