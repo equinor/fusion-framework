f46552c7c7e873214692dd6e46417d7da70d6d65
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-09-27 13:25:31 +0200

    refactor(tests): optimize configurator tests and improve test coverage

--- packages/modules/module/src/tests/configurator.test.ts
+++ packages/modules/module/src/tests/configurator.test.ts
@@ -1,50 +1,38 @@
-import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { ModulesConfigurator } from '../configurator';
-import { type Module } from '../types';
-
-// TODO - allow never ref
-const testModule = {
-    name: 'testModule',
-    configure: vi.fn(async () => ({})),
-    initialize: vi.fn(async () => ({})),
-} satisfies Module<'testModule', object, object>;
-
-// let testConfigurator: ModulesConfigurator;
-let testConfigurator: ModulesConfigurator<[typeof testModule]>;
+import { AnyModule } from '../types';
 
 describe('ModulesConfigurator', () => {
-    beforeAll(() => {
-        testConfigurator = new ModulesConfigurator([testModule]);
-    });
-    afterEach(() => {
-        vi.clearAllMocks();
-    });
-
-    it('should have a configurator with one module registered', () => {
-        expect(testConfigurator).toBeInstanceOf(ModulesConfigurator);
-        expect(testConfigurator.modules).toHaveLength(1);
-    });
 
     it('should allow adding configuration', () => {
-        testConfigurator.addConfig({
+        const configurator = new ModulesConfigurator();
+        configurator.addConfig({
             module: {
-                name: 'tester2',
+                name: 'additionalModule',
                 initialize: () => ({}),
             },
         });
-        expect(testConfigurator.modules).toHaveLength(2);
+        expect(configurator.modules).toHaveLength(1);
+    });
+
+    it('should allow creating with modules', () => {
+        const configurator = new ModulesConfigurator([
+            {
+                name: 'initialModule',
+                initialize: () => ({}),
+            },
+        ]);
+        expect(configurator.modules).toHaveLength(1);
     });
 
     it('should create an instance', async () => {
         const expectedInstance = Symbol('expectedInstance');
-        const module: Module<'shouldCreateInstance', object, object> = {
+        const module: AnyModule = {
             name: 'shouldCreateInstance',
             initialize: vi.fn(async () => expectedInstance),
         };
 
-        const configurator = new ModulesConfigurator([module]);
-
-        const result = await configurator.initialize();
+        const result = await new ModulesConfigurator([module]).initialize();
 
         expect(module.initialize).toHaveBeenCalledOnce();
 
@@ -54,36 +42,30 @@ describe('ModulesConfigurator', () => {
 
     it('should generate module config', async () => {
         const expectedConfig = Symbol('expectedConfig');
-        const configure = vi.fn(async () => expectedConfig);
 
-        testConfigurator.addConfig({
-            module: {
-                name: 'shouldGenerateModuleConfig',
-                configure,
-                initialize: async ({ config }) => {
-                    expect(config).toBe(expectedConfig);
-                    return {};
-                },
+        const module: AnyModule = {
+            name: 'shouldGenerateModuleConfig',
+            configure: vi.fn(async () => expectedConfig),
+            initialize: async ({ config }) => {
+                expect(config).toBe(expectedConfig);
+                return Symbol('instance');
             },
-        });
+        };
 
-        await testConfigurator.initialize();
+        await new ModulesConfigurator([module]).initialize();
 
-        expect(configure).toHaveBeenCalledOnce();
+        expect(module.configure).toHaveBeenCalledOnce();
     });
 
     it('should call postConfigure on modules', async () => {
-        const postConfigure = vi.fn();
-        testConfigurator.addConfig({
-            module: {
-                name: 'shouldCallPostConfigure',
-                initialize: async () => ({}),
-                postConfigure,
-            },
-        });
+        const module: AnyModule = {
+            name: 'shouldCallPostConfigure',
+            initialize: async () => Symbol('instance'),
+            postConfigure: vi.fn(),
+        };
 
-        await testConfigurator.initialize();
+        await new ModulesConfigurator([module]).initialize();
 
-        expect(postConfigure).toHaveBeenCalled();
+        expect(module.postConfigure).toHaveBeenCalled();
     });
 });
