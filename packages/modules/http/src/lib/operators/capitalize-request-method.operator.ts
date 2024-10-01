5c3d4fe0095381323e5a7f132d6a5687670b2825
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2024-10-01 20:56:23 +0200

    refactor(module-http): Refactor capitalizeRequestMethodOperator to allow handle errors silently

--- packages/modules/http/src/lib/operators/capitalize-request-method.operator.ts
+++ packages/modules/http/src/lib/operators/capitalize-request-method.operator.ts
@@ -8,14 +8,15 @@ import type { ProcessOperator } from './types';
  * @returns A new request object with the HTTP method in uppercase.
  */
 export const capitalizeRequestMethodOperator =
-    <T extends RequestInit>(): ProcessOperator<T> =>
+    <T extends RequestInit>(options?: { silent?: boolean }): ProcessOperator<T> =>
     (request): T => {
         const { error, success, data } = requestMethodCasing().safeParse(request.method);
-        if (success) {
-            request.method = data;
-        } else if (error) {
+
+        request.method = success ? data : request.method?.toUpperCase();
+
+        if (error && !options?.silent) {
             error.errors.forEach((e) => console.warn(e.message));
-            request.method = request.method?.toUpperCase();
         }
+
         return request;
     };
