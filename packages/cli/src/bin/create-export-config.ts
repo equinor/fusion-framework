536392d2277f82421eaeb407d8839c716cb83be0
Øyvind Eikeland <43904393+eikeland@users.noreply.github.com>
2024-10-07 14:32:07 +0200

    fix(cli): better error handling of isAppRegistered (#2507)

--- packages/cli/src/bin/create-export-config.ts
+++ packages/cli/src/bin/create-export-config.ts
@@ -19,6 +19,7 @@ import type { FusionEnv } from './utils/index.js';
 import { ConfigExecuterEnv } from '../lib/utils/config.js';
 import { resolveAppKey } from '../lib/app-package.js';
 import { exit } from 'node:process';
+import assert from 'node:assert';
 
 export const createExportConfig = async (options: {
     command?: ConfigExecuterEnv['command'];
@@ -107,12 +108,13 @@ export const createExportConfig = async (options: {
                 );
             }
 
-            await isAppRegistered(state.endpoint, appKey);
+            const exist = await isAppRegistered(state.endpoint);
+            assert(exist, `${appKey} is not registered`);
             spinner.succeed(`${appKey} is registered`);
         } catch (e) {
             const err = e as Error;
             spinner.fail('🙅‍♂️', chalk.bgRed(err.message));
-            exit(1);
+            throw err;
         }
 
         try {
