39b6fca5d1a488f90e380b034579b899059f6f26
Øyvind Eikeland <oyvind@eikeland.me>
2024-10-11 15:24:59 +0200

    fix/packages/modules

--- packages/cli/rollup.config.mjs
+++ packages/cli/rollup.config.mjs
@@ -1,21 +1,23 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default [
-  {
-    input: './src/bin/main.ts',
-    output: {
-      file: './dist/bin/main.mjs',
-      format: 'es'
+    {
+        input: './src/bin/main.ts',
+        output: {
+            file: './dist/bin/main.mjs',
+            format: 'es',
+        },
+        plugins: [
+            typescript({
+                outputToFilesystem: false,
+                compilerOptions: {
+                    composite: false,
+                    declaration: false,
+                    outDir: './dist/bin',
+                    declarationDir: './dist/bin',
+                    sourceMap: false,
+                },
+            }),
+        ],
     },
-    plugins: [typescript({
-      outputToFilesystem: false,
-      compilerOptions: {
-        composite: false,
-        declaration: false,
-        outDir: './dist/bin',
-        declarationDir: './dist/bin',
-        sourceMap: false,
-      }
-    })]
-  },
 ];
