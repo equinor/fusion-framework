64574cee4ad14edee3f704a520b269d605e1a3e7
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-02-12 14:29:43 +0100

    feat(framework): update useCurrentUser to integrate new MSAL provider interface

---
+++ .changeset/five-pets-design.md
@@ -0,0 +1,5 @@
+---
+'@equinor/fusion-framework-react': minor
+---
+
+Updated `useCurrentUser` to consume new MSAL provider interface
--- packages/react/framework/src/hooks/use-current-user.ts
+++ packages/react/framework/src/hooks/use-current-user.ts
@@ -1,15 +1,7 @@
+import { AccountInfo } from '@equinor/fusion-framework-module-msal';
 import { useFramework } from '../useFramework';
-
-export type AccountInfo = {
-    // homeAccountId: string;
-    environment: string;
-    tenantId: string;
-    username: string;
-    localAccountId: string;
-    name?: string;
-};
 
 export const useCurrentUser = (): AccountInfo | undefined => {
     const framework = useFramework();
-    return framework.modules.auth.defaultClient.account;
+    return framework.modules.auth.defaultAccount;
 };
