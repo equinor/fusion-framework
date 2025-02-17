97c90623daa6d0e887daf273e00a45f3b3e8edb9
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-02-17 10:16:36 +0100

    refactor: remove unused theme properties from fusionTheme

---
+++ .changeset/breezy-buses-compare.md
@@ -0,0 +1,5 @@
+---
+'@equinor/fusion-framework-module-ag-grid': patch
+---
+
+Hotfix to remove demo styling of ag-grid.
--- packages/modules/ag-grid/src/themes.ts
+++ packages/modules/ag-grid/src/themes.ts
@@ -2,31 +2,6 @@ import { type Theme, themeAlpine, createTheme, createPart } from 'ag-grid-commun
 
 export const fusionTheme: Theme = themeAlpine.withParams({
     fontFamily: 'Equinor, sans-serif',
-    accentColor: '#00A2FF',
-    backgroundColor: '#21222C',
-    borderColor: '#429356',
-    borderRadius: 0,
-    browserColorScheme: 'dark',
-    cellHorizontalPaddingScale: 0.8,
-    cellTextColor: '#50F178',
-    columnBorder: true,
-    fontSize: 12,
-    foregroundColor: '#68FF8E',
-    headerBackgroundColor: '#21222C',
-    headerFontSize: 14,
-    headerFontWeight: 700,
-    headerTextColor: '#68FF8E',
-    headerVerticalPaddingScale: 1.5,
-    oddRowBackgroundColor: '#21222C',
-    rangeSelectionBackgroundColor: '#FFFF0020',
-    rangeSelectionBorderColor: 'yellow',
-    rangeSelectionBorderStyle: 'dashed',
-    rowBorder: true,
-    rowVerticalPaddingScale: 1.5,
-    sidePanelBorder: true,
-    spacing: 4,
-    wrapperBorder: true,
-    wrapperBorderRadius: 0,
 });
 
 export const createThemeFromTheme = (theme: Theme): Theme => {
