463f06e4c4b1bcf1372ba22afc61b63db7b9ef2d
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-03-10 14:53:52 +0100

    refactor: align code with Biome rule `noAccumulatingSpread` and enhance documentation for OData functions

--- packages/modules/services/src/context/index.ts
+++ packages/modules/services/src/context/index.ts
@@ -2,5 +2,7 @@ export { ContextApiClient, default } from './client';
 
 export { ApiVersion } from './static';
 
+export { ApiContextEntity } from './api-models';
+
 export * from './api-models';
 export * from './types';
