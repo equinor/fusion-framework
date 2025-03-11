4ab72a727dda0f678384e6a5e88b0fd20c170a3b
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-03-11 09:38:17 +0100

    refactor(tests): enhance mock context item with date normalization and improve seed generation

--- packages/modules/services/tests/mocks/get-context-item.ts
+++ packages/modules/services/tests/mocks/get-context-item.ts
@@ -1,24 +1,48 @@
-import { Faker, en } from '@faker-js/faker';
+import { Faker, da, en } from '@faker-js/faker';
 import { type ApiContextEntity, ApiVersion } from '../../src/context';
 
 import { createHash } from 'node:crypto';
 
-function uuidToSeed(uuid: string): number {
+/**
+ * Converts a string to a seed for the Faker.js library.
+ *
+ * @param uuid - The UUID string to convert.
+ * @returns A numeric seed derived from the UUID.
+ */
+function stringToSeed(uuid: string): number {
   const hash = createHash('sha256').update(uuid).digest('hex');
   return Number.parseInt(hash.slice(0, 8), 16);
+}
+
+/**
+ * Since generation of dates might have a slight difference in milliseconds
+ * we normalize the date to the nearest minute, so we can compare them.
+ * 
+ * @note
+ * This might fail at exactly midnight, but the odds for that are slim. Rerun the test if it fails.
+ * 
+ * @param date date to normalize
+ * @returns string
+ */
+function normalizeDateMock(date: Date): string {
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date.toISOString();
 }
 
 export const mockContextItem = <V extends ApiVersion = ApiVersion.v1>(
   id: string,
   version: V = ApiVersion.v1 as V,
 ): ApiContextEntity<V> => {
-  const faker = new Faker({ seed: uuidToSeed(id), locale: en });
+  const seed = stringToSeed(id);
+  const faker = new Faker({ seed, locale: en });
   switch (version) {
     case ApiVersion.v1:
       return {
         id: String(id),
         externalId: faker.string.uuid(),
-        created: faker.date.recent().toUTCString(),
+        created: normalizeDateMock(faker.date.recent()),
         source: faker.string.uuid(),
         type: {
           id: faker.string.uuid(),
@@ -29,7 +53,7 @@ export const mockContextItem = <V extends ApiVersion = ApiVersion.v1>(
         title: faker.lorem.sentence(),
         isActive: true,
         isDeleted: false,
-        updated: faker.date.past().toUTCString(),
+        updated: normalizeDateMock(faker.date.past()),
       } satisfies ApiContextEntity<ApiVersion.v1> as ApiContextEntity<V>;
   }
   throw new Error('Not implemented');
