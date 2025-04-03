1047de5a5d3a7e6b6148436722ab8a1540fe2146
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-04-03 09:01:31 +0200

    feat(spa-plugin): add initial implementation of Fusion Framework Vite SPA Plugin (#2939)
    * feat(spa-plugin): add initial implementation of Fusion Framework Vite SPA Plugin
    
    * style: update line endings

---
+++ packages/vite-plugins/spa/src/html/index.html.ts
@@ -0,0 +1,42 @@
+import { version } from '../version.js';
+
+/**
+ * Represents an HTML template string used for generating the main structure of an SPA (Single Page Application).
+ *
+ * @see {@link https://vite.dev/guide/env-and-mode.html#html-constant-replacement}
+ *
+ * The template includes placeholders for dynamic values such as:
+ * - `%FUSION_SPA_TITLE%`: The title of the SPA.
+ * - `%MODE%`: The mode of the application (e.g., development, production).
+ * - `%FUSION_SPA_BOOTSTRAP%`: The path to the bootstrap script for initializing the SPA.
+ *
+ * Additionally, it includes:
+ * - A meta tag for the plugin version, dynamically populated using the `version` variable.
+ * - A link to the Equinor font stylesheet hosted on a CDN.
+ *
+ * @constant
+ * @type {string}
+ */
+export const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>%FUSION_SPA_TITLE%</title>
+      <meta name="mode" content="%MODE%">
+      <meta name="fusion-spa-plugin-version" content="${version}">
+      <script type="module" src="%FUSION_SPA_BOOTSTRAP%"></script>
+      <link rel="stylesheet" href="https://cdn.eds.equinor.com/font/equinor-font.css" />
+      <style>
+        html, body {
+          margin: 0;
+          padding: 0;
+          height: 100%;
+          font-family: 'EquinorFont', sans-serif;
+        }
+      </style>
+    </head>
+    <body></body>
+  </html>
+`;
+
+export default html;
---
+++ packages/vite-plugins/spa/src/html/index.ts
@@ -0,0 +1,1 @@
+export { registerServiceWorker } from './register-service-worker.js';
