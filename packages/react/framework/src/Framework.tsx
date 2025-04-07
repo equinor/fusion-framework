61460a05a249ff5df973a85e536061e9e7a7c3b8
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-04-07 11:04:37 +0200

    feat: add optional parent ModulesInstance to Framework component

--- packages/react/framework/src/Framework.tsx
+++ packages/react/framework/src/Framework.tsx
@@ -2,6 +2,7 @@ import type { FrameworkConfigurator } from '@equinor/fusion-framework';
 import { createFrameworkProvider } from './create-framework-provider';
 import { type PropsWithChildren, type ReactNode, Suspense, useMemo } from 'react';
 import { useModules } from '@equinor/fusion-framework-react-module';
+import type { ModulesInstance } from '@equinor/fusion-framework-module';
 
 type ConfigureCallback = (configurator: FrameworkConfigurator) => void;
 
@@ -9,12 +10,17 @@ export const Framework = (
   props: PropsWithChildren<{
     readonly configure: ConfigureCallback;
     readonly fallback: NonNullable<ReactNode> | null;
+    // biome-ignore lint/suspicious/noExplicitAny: should alow any
+    readonly parent?: ModulesInstance<any>;
   }>,
 ) => {
-  const { configure, fallback, children } = props;
+  const { configure, fallback, parent, children } = props;
   //import modules from parent context
   const ref = useModules<[]>();
-  const Component = useMemo(() => createFrameworkProvider(configure, ref), [configure, ref]);
+  const Component = useMemo(
+    () => createFrameworkProvider(configure, parent ?? ref),
+    [configure, ref, parent],
+  );
   return (
     <Suspense fallback={fallback}>
       <Component>{children}</Component>
