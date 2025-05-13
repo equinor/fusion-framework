a6b235386560d80a7a21d0b1a85251f87292ed79
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-05-13 10:31:49 +0200

    feat: enhance importScript function with improved error handling and support for output file management

--- packages/utils/imports/src/import-script.ts
+++ packages/utils/imports/src/import-script.ts
@@ -1,6 +1,9 @@
 import { build, type BuildOptions } from 'esbuild';
 import { access } from 'node:fs/promises';
 import path from 'node:path';
+import { processAccessError } from './error.js';
+
+import { readPackageUp } from 'read-package-up';
 
 /**
  * Represents a Node.js module with an optional default export.
@@ -16,13 +19,14 @@ export type EsmModule = Record<string, unknown> & {
 /**
  * Options for importing a script, excluding certain build options.
  *
+ * @note when `write` is set to `false`, the output will be loaded from memory.
+ *
  * This type is derived from `BuildOptions` but omits the following properties:
  * - `entryPoints`
- * - `write`
  * - `bundle`
  * - `format`
  */
-export type ImportScriptOptions = Omit<BuildOptions, 'entryPoints' | 'write' | 'bundle' | 'format'>;
+export type ImportScriptOptions = Omit<BuildOptions, 'entryPoints' | 'bundle' | 'format'>;
 
 /**
  * Asynchronously imports a script file using esbuild to bundle it.
@@ -52,39 +56,56 @@ export const importScript = async <M extends EsmModule>(
   try {
     await access(sourceFile);
   } catch (error) {
-    throw new Error(`The file '${entryPoint}' does not exist or cannot be accessed`, {
-      cause: error,
-    });
+    throw processAccessError(error, sourceFile);
   }
+
+  const packageLocalPath = await readPackageUp().then((pkg) =>
+    pkg ? path.dirname(pkg?.path) : process.cwd(),
+  );
+
+  const outfile =
+    options?.outfile ??
+    path.join(
+      packageLocalPath,
+      'node_modules/.cache/esbuild',
+      `${path.basename(sourceFile)}.esbuild.js`,
+    );
 
   try {
     const buildOptions = Object.assign(
       {
         // default options
+        outfile,
         platform: 'node',
+        write: true,
       },
       options, // provided options
       {
         // non-overridable options
+        outdir: undefined,
         entryPoints: [sourceFile],
-        write: false,
         bundle: true,
+        packages: 'external',
         format: 'esm',
       },
     ) as BuildOptions;
 
     const result = await build(buildOptions);
 
-    // Ensure that at least one output file was generated
-    const [output] = result.outputFiles ?? [];
-    if (!output) {
-      throw new Error(`No output files generated for '${entryPoint}'`);
+    if (!buildOptions.write) {
+      // If write is false, we need to write the output files manually
+      // Ensure that at least one output file was generated
+      const [output] = result.outputFiles ?? [];
+      if (!output) {
+        throw new Error(`No output files generated for '${entryPoint}'`);
+      }
+
+      // Create a data URL from the bundled script
+      const dataUrl = `data:text/javascript;base64,${Buffer.from(output.text).toString('base64')}`;
+      return import(dataUrl);
     }
 
-    // Create a data URL from the bundled script
-    const dataUrl = `data:text/javascript;base64,${Buffer.from(output.text).toString('base64')}`;
-
-    return import(dataUrl);
+    return import(outfile);
   } catch (error) {
     throw new Error(
       `Failed to bundle '${entryPoint}' with esbuild. Check for syntax errors or unresolved imports.`,
