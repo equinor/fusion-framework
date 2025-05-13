6d14d20d28780647b71edf8459b81e472a25e225
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-05-13 10:32:32 +0200

    feat: add read-package-up dependency and update import paths to include file extensions

--- packages/utils/imports/src/index.ts
+++ packages/utils/imports/src/index.ts
@@ -1,4 +1,11 @@
-export { importScript } from './import-script';
-export { importJSON } from './import-json';
-export { default, importConfig, type ConfigContent } from './import-config';
-export { resolveConfigFile } from './resolve-config-file';
+export { importScript } from './import-script.js';
+export { importJSON } from './import-json.js';
+export {
+  default,
+  importConfig,
+  type ConfigContent,
+  type ImportConfigResult,
+} from './import-config.js';
+export { resolveConfigFile } from './resolve-config-file.js';
+
+export { FileNotFoundError, FileNotAccessibleError } from './error.js';
