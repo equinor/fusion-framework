a4d6023d4c18c50bbb657e638ffd07ea3ca84f40
Øyvind Eikeland <oyvind@eikeland.me>
2025-05-14 09:47:08 +0200

    feat(apploader): adding app embed component and cookbook

---
+++ cookbooks/app-react-apploader/README.md
@@ -0,0 +1,3 @@
+# React Apploader cookbook
+
+App for loading other apps inside you app when cooking with Fusion-Framework
---
+++ packages/react/app/src/fusion-apploader/FusionApploader.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react';
+import { useAppLoader } from './useAppLoader';
+
+export type FusionApploaderProps = {
+  appKey: string;
+}
+
+export const FusionApploader = ({appKey}: FusionApploaderProps): JSX.Element => {
+  const refWrapp = useRef<HTMLDivElement | null>(null);
+  const { loading, error, appRef } = useAppLoader({appKey});
+
+  useEffect(() => {
+    if (!refWrapp.current || !appRef.current) {
+      return
+    };
+    
+    refWrapp.current.appendChild(appRef.current);
+  }, [appRef.current]);
+  
+  if (loading) {
+    return <div>Loading {appKey}</div>;
+  }
+  
+  if (error) {
+    return <div>Error loading {appKey}. Error: {error.message}</div>;
+  }
+
+  return <div ref={refWrapp}></div>;
+};
+
+export default FusionApploader;
