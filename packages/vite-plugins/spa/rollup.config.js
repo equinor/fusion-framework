ea96493d95336f682e31a7b63161783ae7c99a63
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2025-06-10 11:56:14 +0200

    refactor(vite-plugin-spa): update build output and entrypoints for clarity

--- packages/vite-plugins/spa/rollup.config.js
+++ packages/vite-plugins/spa/rollup.config.js
@@ -1,23 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
 
 /** @type {import('rollup').RollupOptions} */
 export default [
   {
     input: {
-      plugin: 'src/plugin.ts',
-      'html/bootstrap': 'src/html/bootstrap.ts',
-      'html/sw': 'src/html/sw.ts',
+      'index': 'dist/esm/index.js',
+      'html/bootstrap': 'dist/esm/html/bootstrap.js',
+      'html/sw': 'dist/esm/html/sw.js',
     },
     output: {
       format: 'esm',
-      dir: 'dist',
+      dir: 'dist/bin',
       entryFileNames: '[name].js',
       sourcemap: true,
       strict: false,
     },
     external: ['fsevents', 'jiti', 'lightningcss', 'yaml'],
-    plugins: [typescript({ declaration: true }), resolve({ preferBuiltins: true }), commonjs()],
+    plugins: [resolve({ preferBuiltins: true }), commonjs()],
   },
 ];
