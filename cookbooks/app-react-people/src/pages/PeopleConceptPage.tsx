2d513df35add5f10c52c2f24dda24d93ddc5cdde
Øyvind Eikeland <oyvind@eikeland.me>
2026-03-12 14:18:03 +0100

    fix: update routes to include PeopleConceptPage and ensure proper export

--- cookbooks/app-react-people/src/pages/PeopleConceptPage.tsx
+++ cookbooks/app-react-people/src/pages/PeopleConceptPage.tsx
@@ -100,3 +100,5 @@ export const PeopleConceptPage = () => {
     </div>
   );
 };
+
+export default PeopleConceptPage;
--- cookbooks/app-react-people/src/routes.tsx
+++ cookbooks/app-react-people/src/routes.tsx
@@ -1,13 +1,12 @@
 import { layout, index, route } from '@equinor/fusion-framework-react-router/routes';
 
-// TODO
-// route('people-concepts', './pages/PeopleConceptPage.tsx'),
 export const routes = layout('./pages/Navigation.tsx', [
   index('./pages/HomePage.tsx'),
   route('avatar/*', './pages/AvatarPage.tsx'),
   route('card/*', './pages/CardPage.tsx'),
   route('list-item/*', './pages/ListItemPage.tsx'),
   route('selector/*', './pages/SelectorPage.tsx'),
+  route('people-concepts/*', './pages/PeopleConceptPage.tsx'),
 ]);
 
 export default routes;
