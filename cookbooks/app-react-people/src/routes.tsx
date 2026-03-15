7b932573c6eb20075a819d28caeaa386bce1a897
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2026-03-15 17:04:08 +0100

    chore: resolves https://github.com/equinor/fusion-framework/pull/4113

--- cookbooks/app-react-people/src/routes.tsx
+++ cookbooks/app-react-people/src/routes.tsx
@@ -1,7 +1,5 @@
 import { layout, index, route } from '@equinor/fusion-framework-react-router/routes';
 
-// TODO
-// route('people-concepts', './pages/PeopleConceptPage.tsx'),
 export const routes = layout('./pages/Navigation.tsx', [
   index('./pages/HomePage.tsx'),
   route('avatar/*', './pages/AvatarPage.tsx'),
