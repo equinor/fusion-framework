c62f30346fbf9907799a87f7432730f7d59e5fa2
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2026-05-05 12:10:06 +0200

    docs(react-module-event): add README and @packageDocumentation TSDoc
    Add README covering useEventHandler, useEventStream, EventConsumer,
    and EventProvider with examples. Add @packageDocumentation to src/index.ts.
    
    Closes equinor/fusion-core-tasks#877

--- packages/react/modules/event/src/index.ts
+++ packages/react/modules/event/src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * React integration for the Fusion event module.
+ *
+ * Provides hooks ({@link useEventHandler}, {@link useEventStream}) and
+ * React context components ({@link EventConsumer}, {@link EventProvider})
+ * for subscribing to and dispatching framework events from React components.
+ *
+ * @packageDocumentation
+ */
 export * from '@equinor/fusion-framework-module-event';
 
 export { EventConsumer, EventProvider } from './eventContext';
