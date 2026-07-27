22027f7f96556105947e353519c31bbc9bda002f
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2026-07-27 12:47:37 +0200

    chore(react-module-signalr): add missing tsdoc comments

--- packages/react/modules/signalr/src/use-signalr.ts
+++ packages/react/modules/signalr/src/use-signalr.ts
@@ -3,6 +3,14 @@ import { moduleKey } from '@equinor/fusion-framework-module-signalr';
 
 import { useSignalRProvider } from './use-signalr-provider';
 
+/**
+ * Hook for connecting to a SignalR hub topic, resolving the provider from the framework automatically.
+ *
+ * @template T - The type of data emitted by the topic
+ * @param hubId - The id of the hub to connect to
+ * @param topicId - The id of the topic within the hub to subscribe to
+ * @returns The connected topic
+ */
 export const useTopic = <T>(
   hubId: string,
   topicId: string,
